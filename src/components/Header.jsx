6732503697469588f0930d7950f4774117348d65
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-23 14:58:06 -0300

    Implementaçao de com ponente Header com titulo da pagina(Requisito 01)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,17 @@
 // implement Header component here.
+import React, { Component } from 'react';
+import '../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="movie-card-header">
+
+        <h1 className="page-title">Movie Cards Library</h1>
+
+      </header>
+    );
+  }
+}
+
+export default Header;

a36fb97998f1f864769d9408c1e6b409acfa4432
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 17:04:58 -0300

    rating classes for styling

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,11 @@ import PropTypes from 'prop-types';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    return <div className="rating">{rating}</div>;
+    return (
+      <div className="movie-card-rating">
+        <span className="rating">{rating}</span>
+      </div>
+    );
   }
 }
 

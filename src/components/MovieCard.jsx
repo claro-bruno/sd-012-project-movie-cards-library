7146a3c262ff4c8b1569d48e59ad54068b17b442
Elias Forte <eliasforte.dev@gmail.com>
2021-06-23 15:17:35 -0300

    requisito 6,

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (<h1>teste</h1>);
+  }
+}
+
+export default MovieCard;

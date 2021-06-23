f1ec1d75f598d507b6ea8667cc7123c82bf35e7e
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-23 15:15:50 -0300

    Requisito 1 e 2 - Implementa o Header da pagina com o titulo "Movie Cards Library"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
 // implement Header component here
 // Iniciando o projeto 🚀
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;

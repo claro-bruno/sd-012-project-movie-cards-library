7670fbb96b027dc18a38c7472d056079e3cebabc
Erick Silva <erick.silva6653@gmail.com>
2021-06-23 14:54:59 -0300

    componenteRating pronto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }

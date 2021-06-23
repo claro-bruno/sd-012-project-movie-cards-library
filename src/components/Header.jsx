ba4ed6e99f13bf97b741aab20de7ee091a62f10b
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-06-23 17:55:27 -0300

    adiciona requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <header />;
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
   }
 }
 

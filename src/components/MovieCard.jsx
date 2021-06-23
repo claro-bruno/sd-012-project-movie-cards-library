a822fbfa8e8b58f262d64e011690f063e512207a
CamilaDamasio <camiladamasioef@hotmail.com>
2021-06-23 19:36:15 -0300

    Verifica requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,6 +14,9 @@ class MovieCard extends React.Component {
           <h5>
             { movie.subtitle }
           </h5>
+          <p>
+            { movie.storyline }
+          </p>
         </section>
       </div>);
   }

f7c57c4ddaf6b81682f769fbd7d42a2993c9d845
alestori <alestori7@gmail.com>
2021-06-23 12:05:40 -0300

    Reqs 3 & 4: agora sim renderiza os MovieCards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+const MovieCard = (props) => {
+  console.log(props);
+  return (
+    <li>
+      <h4>bla</h4>
+    </li>
+  );
+};
+
+export default MovieCard;

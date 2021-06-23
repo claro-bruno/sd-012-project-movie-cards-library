3c1910e7f7d0c955e6252aafd4e78819e9843708
matheustkaczyk <matheus111904@hotmail.com>
2021-06-23 16:20:56 -0300

    correcoes lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import MovieCard from './MovieCard';
 
-class MovieList extends React.Component{
-    render(){
-      const { movies } = this.props;
-    return(
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
       <div>
         { movies.map((movie) => <MovieCard key={movie.title} movies={movie}/>) }
       </div>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 class Rating extends React.Component{
     render(){
         const { rating } = this.props
-        return(
 
-        )
     }
 }

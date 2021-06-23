4f69eee3e119db43e53602581337e4889a193a5b
Milton Castro <castro.milton07@gmail.com>
2021-06-23 18:57:46 -0300

    Requisito 1 - Cria um componente <Header />

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header />
+    );
+  }
+}
+
+export default Header;

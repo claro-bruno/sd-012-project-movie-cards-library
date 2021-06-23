a9bb0b159adcb34874fdac92186a3b0504409342
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 16:01:55 -0300

    requisito 9 - Renderize o subtítulo do filme dentro de uma tag `h5`

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,6 +10,7 @@ class MovieCard extends React.Component {
       <div>
         <img src={ imagePath } alt={ `Banner for ${title}` } />
         <h4>{title}</h4>
+        <h5>{subtitle}</h5>
       </div>
     );
   }

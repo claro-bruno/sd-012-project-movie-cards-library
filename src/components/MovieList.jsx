6d3fb9df69cced693c73bc9bd39a6635caf1b727
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-23 16:01:20 -0300

    Adiciona CSS no componente <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,11 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return (movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
+    return (
+      <main className="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </main>
+    );
   }
 }
 

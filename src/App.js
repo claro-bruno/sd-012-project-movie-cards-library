44ec5fc50ad731f81f5dc24e4a504a0388d6c471
Gabriel Viana <g.viana87@hotmail.com>
2021-06-23 15:50:26 -0300

    Item 1 - Crie um componente <Header /> e item 2 - Renderize um texto no <Header />

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;

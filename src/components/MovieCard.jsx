38a0757f2520af7455fcd1718f0545e93aa8fbaf
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-23 17:21:15 -0300

    Adicionadas classes a todas as tag renderizadas no componente; criada a div com a classe movie-card-boy encapsulando todo o texto do card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,11 +9,13 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section className="movie-card">
-        <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
+        <img src={ imagePath } alt={ title } className="movie-card-image " />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } className="movie-card-rating" />
+        </div>
       </section>
     );
   }

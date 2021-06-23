a0e1bdb7c94ba8b733cc42daacca2857d21e6e05
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-06-23 17:14:57 -0300

    Corrigindo alguns erros acusados pelo ESLint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { movie } = this.props;
 
     return (
       <section>
-        <h1>{ title }</h1>
+        <h1>{ movie.title }</h1>
       </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;

d551539b2a1b819f7513829a88c5d85e516c72c8
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-06-23 15:17:00 -0300

    comecando projeto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+
+};
+export default Header;

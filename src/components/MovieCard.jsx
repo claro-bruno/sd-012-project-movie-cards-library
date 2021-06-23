8981194ad8dee3652a67b5423f8cd483c81d5ce4
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-06-23 16:42:26 -0300

    prepara component MovieList para receber o component MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <h1>teste MovieCard</h1>;
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        {
+          movies.map((movie) => <MovieCard key={ movie.id } MovieInfo={ movie } />)
+        }
+      </section>);
+  }
+}
+
+export default MovieList;

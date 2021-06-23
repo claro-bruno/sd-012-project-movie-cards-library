13d4e43adfc501b6491e2bcf58bedd6c8afec76d
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 14:59:16 -0300

    requisito 3 - Crie um componente `<MovieList />`

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return <section />;
+  }
+}
+
+export default MovieList;

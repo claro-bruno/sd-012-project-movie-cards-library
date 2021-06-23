e4adae06da23281cd7fe0b0efb8b2efd0009f14e
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-06-23 16:37:54 -0300

    requisito 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <h4>Nome do filme</h4>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <section>
+        {
+          movies.map((movieCard) => <MovieCard key={ movieCard } />)
+        }
+      </section>
+    );
+  }
+}
+
+export default MovieList;

81bd7983974942f20ccb9dab3bb9c9a2b2e4cacd
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-23 15:55:32 -0300

    Requisitos 6, 7, 8, 9 e 10
    6 - Cria um componente `<MovieCard />`
    7 - Renderiza a imagem do filme dentro de uma tag `img`
    8 - Renderiza o título do filme dentro de uma tag `h4`
    9 - Renderiza o subtítulo do filme dentro de uma tag `h5`
    10 - Renderiza a sinopse do filme dentro de uma tag `p`

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <section className="movie-card">
+        <img src={ movie.imagePath } alt={ movie.title } className="movie-card-image" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline ">{ movie.storyline }</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.exact({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return (movies.map((movie) => <MovieCard key={ movie.title } />));
+    return (movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
   }
 }
 

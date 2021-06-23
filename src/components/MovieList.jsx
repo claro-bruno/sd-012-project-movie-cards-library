88ff4110ffa925320d18a0ae643576b7a2711c5f
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-06-23 16:20:09 -0300

    Criando o componente MovieList e renderizando cada MovieCard dentro dele.

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+
+    const { movies } = this.props;
+
+    return (
+      <main>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </main>
+    );
+  }
+}
+
+export default MovieList;

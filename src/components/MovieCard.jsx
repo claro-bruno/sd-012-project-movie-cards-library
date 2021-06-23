0de3f0a4ca3f183eb37d428f72a3151a4131909a
camila-mp <ca.malvp@gmail.com>
2021-06-23 18:56:33 -0300

    resolve requisitos 11, 12, 13, 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline } = movie;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
     return (
       <div className="movieCard">
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 

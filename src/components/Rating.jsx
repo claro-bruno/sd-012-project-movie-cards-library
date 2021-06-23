95cfdd768b64ac6fbb39a6bd10de94b7797c2f80
Eric Kreis <erickreis793@gmail.com>
2021-06-23 13:17:17 -0300

    Requisitos de 11 a 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../App.css';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return <section className="rating">{ rating }</section>;
+  }
+}
+
+Rating.defaultProps = {
+  rating: 5,
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+export default Rating;

ee8c310727e01cdde65e57440c6aa0a115edcad9
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-23 15:03:01 -0300

    Criado componente Movie Card (Requisito 6)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+    )
+  }
+}
+
+export default MovieCard;

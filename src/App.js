2418ccddab6fa95a6a9ec921c6d8e63ee3b1350f
André Dantas <aluizcfd@gmail.com>
2021-06-23 18:49:26 -0300

    Adição do component Header.jsx em App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import movies from './data';
+import Header from './components/Header';
 import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList movies={ movies } />
     </div>
   );

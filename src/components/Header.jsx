030da71c1db428111bfd3593118edc9f6a8b168b
Martin <escorpmartin97@gmail.com>
2021-06-23 18:08:12 -0300

    1 - Crie um componente Header e 2 - Renderize um texto no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  reder() {
+    return (
+      <h1> </h1>
+    );
+  }
+}
+
+export default MovieList;

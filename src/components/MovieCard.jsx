e73367a6c3ca27d6a7a65a6600ea49a4b583ebc9
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 16:55:41 -0300

    moviecard classes for styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <div>
+      <div className="movie-card">
         <img src={ imagePath } alt={ `Banner for ${title}` } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>

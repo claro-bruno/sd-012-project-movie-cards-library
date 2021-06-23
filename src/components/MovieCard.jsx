6282859c916d50917088f5ac2be7a1a9e150c6bd
Rodrigo <rodrigo.facury14@gmail.com>
2021-06-23 19:00:05 -0300

    Estiliza cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,9 +9,11 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img src={ imagePath } alt={ title } className="movie-card-image" />
-        <h4 className="movie-card-title">{ title }</h4>
-        <h5 className="movie-card-subtitle">{ subtitle }</h5>
-        <p className="movie-card-storyline">{ storyline }</p>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
         <Rating rating={ rating } />
       </div>
     );

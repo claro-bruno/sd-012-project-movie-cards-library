adbab0aa490347f2fe5c819322bea04bbcae8e30
André Dantas <aluizcfd@gmail.com>
2021-06-23 17:44:43 -0300

    Adição do component MovieCard.jsx, somente estrutura.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h1>Texto de placeholder</h1>
+    );
+  }
+}
+
+export default MovieCard;

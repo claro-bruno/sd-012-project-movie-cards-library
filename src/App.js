d07b7ed9bcadefd37323ccb802a5fc6e566b654c
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-06-23 15:48:11 -0300

    renderizando os filmes na pagina, porem com erros de lint ainda a serem corrigidos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import data from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ data } />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+import movie from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;

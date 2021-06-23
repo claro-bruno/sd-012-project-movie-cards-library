a24b89c9b77ead4fc4e0568757d4d30a5a4b6deb
Raphael <raphael.ph2ll@gmail.com>
2021-06-23 18:56:33 -0300

    Resolve ex07-10 - Movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,10 +5,18 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <h1>{movie.title}</h1>
+      <section className="movie-card">
+        <img src={ movie.imagePath } alt={ movie.title } className="movie-card-image" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline ">{ movie.storyline }</p>
+        </div>
+      </section>
     );
   }
 }
+
 MovieCard.propTypes = {
   movie: PropTypes.exact({
     title: PropTypes.string,
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,9 +6,11 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <main className="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </main>
+      <div className="movie-list">
+        {
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
+      </div>
     );
   }
 }

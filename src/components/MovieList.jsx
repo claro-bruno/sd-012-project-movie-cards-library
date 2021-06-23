8e30e87f82c4371dc0511f3ca97daccc1ef954a2
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-06-23 16:18:57 -0300

    resolvendo o lint e problema com json e criando um componente <MovieList />

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        <MovieCard />
+      </section>
+    );
+  }
+}
+
+export default MovieList;

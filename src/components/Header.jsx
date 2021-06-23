c07c5c99dbaa01ed76f93049453ff3c888e26eb4
CamilaDamasio <camiladamasioef@hotmail.com>
2021-06-23 14:48:51 -0300

    Verifica requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
 // implement Header component here
+import React from 'react';
+class Header extends React.Component {
+    render() {
+      return (<Header></Header>);
+    }
+  }
+
+  export default Header;

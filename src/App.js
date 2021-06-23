328a0fc6b7a16407926dbb4dc9bba9905ad7983a
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-06-23 18:33:44 -0300

    adiciona requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import catalog from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <main>
       <Header />
+      <MovieList movie={ catalog[0] } />
+      <MovieList movie={ catalog[1] } />
+      <MovieList movie={ catalog[2] } />
+      <MovieList movie={ catalog[3] } />
+      <MovieList movie={ catalog[4] } />
       <div className="App" />
     </main>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section />
+    );
+  }
+}
+
+export default MovieList;

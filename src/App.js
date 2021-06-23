0b31b022f2d1ddb750bc036ba27ac986f38d85de
André Moreno <deco616@hotmail.com>
2021-06-23 15:04:32 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Headers from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Headers />
     </div>
   );
 }

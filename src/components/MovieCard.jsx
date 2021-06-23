ccf32f32b909ca226010aaed203707c6ba8e3939
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-06-23 17:10:45 -0300

    tentativa de fazer movielist e moviecard funcionarem

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating} } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        {/* <Rating rating={ rating } /> */}
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    // rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;

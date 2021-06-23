642a92fbb7f7f1d3e023c30d3c3080a051c400d1
matheus oliveira de proenca <mproenca.marketing@gmail.com>
2021-06-23 15:25:27 -0300

    adiciona header do requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component her
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;

18a8c448bab741fc7522d2576184710450a9e2bb
ladcs <hpladcs@gmail.com>
2021-06-23 14:54:51 -0300

    VQV

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );

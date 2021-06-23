83ba04ff080ef65dd29ae220333e718e4c0bb8b9
Elias Forte <eliasforte.dev@gmail.com>
2021-06-23 17:11:01 -0300

    requisitos dos 4 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import Proptypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    return (<h1>teste</h1>);
+    const { movie } = this.props;
+    return (
+      <section>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+      </section>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: Proptypes.shape({
+    title: Proptypes.string,
+    subtitle: Proptypes.string,
+    imagePath: Proptypes.string,
+    storyline: Proptypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import Proptypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
-      <MovieCard />);
+      <section>
+        {
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
+      </section>
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: Proptypes.arrayOf(Proptypes.object).isRequired,
+};
 
 export default MovieList;

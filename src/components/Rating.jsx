ec7000e76153fb9e558685c7b3454eb72cb384f0
Caio César <caiovg32@gmail.com>
2021-06-23 17:30:28 -0300

    Req. 13 e 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Rating extends React.Component {
+export default class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
@@ -13,5 +13,3 @@ class Rating extends React.Component {
 Rating.propTypes = {
   rating: PropTypes.number.isRequired,
 };
-
-export default Rating;

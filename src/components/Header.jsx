a75b19ba632fec134ca185a645f55f77fefe057e
Rodrigo <rodrigo.facury14@gmail.com>
2021-06-23 19:10:17 -0300

    Adiciona referencia

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ class Header extends React.Component {
     return (
       <header className="movie-card-header">
         <h1 className="page-title">Movie Cards Library</h1>
+        {/* className adicionada de acordo o projeto da colega Adriana Biberg */}
       </header>);
   }
 }

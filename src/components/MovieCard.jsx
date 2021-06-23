88cda537f703600f9a412a0440f397673a8daac4
André Dantas <aluizcfd@gmail.com>
2021-06-23 18:25:00 -0300

    Adição do conteúdo do component MovieCard, card dos filmes criado puxando os dados de data.js através de props

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+
 class MovieCard extends React.Component {
   render() {
     return (
-      <h1>Texto de placeholder</h1>
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </div>
     );
   }
 }

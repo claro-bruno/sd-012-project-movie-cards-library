536888ad2dbb630dd8aa8d07775810250dc28608
Daniel <danielbped@gmail.com>
2021-06-23 16:34:28 -0300

    Adiciona as classes CSS

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <main>
+      <main className="movie-list">
         {
           movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
         }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types';
 class Rating extends Component {
   render() {
     const { rating } = this.props;
-    return (<div className="rating">{rating}</div>);
+    return (
+      <div className="movie-card-rating">
+        <p className="rating">{rating}</p>
+      </div>);
   }
 }
 

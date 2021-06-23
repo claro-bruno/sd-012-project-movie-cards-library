ffb62f4c253da4ddf69b017901cdab164a4933d6
Rafael Cardoso <rafael.vimur@gmail.com>
2021-06-23 15:58:13 -0300

    adiciona requisitos 3 ao 6 e 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (
+			<h1>Teste</h1>
+    );
+  }
+}
+
+export default MovieCard;

d36bf4d60e8249a7edb78441d74190b27006ef40
José Carlos <kalarruda@hotmail.com>
2021-06-23 19:48:07 -0300

    requisito 12, 13 e 14

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import movies from './data';
 import MovieList from './components/MovieList';
+import Rating from './components/Rating';
 
 import Header from './components/Header';
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList movies={ movies } />
+      <Rating />
     </div>
   );
 }

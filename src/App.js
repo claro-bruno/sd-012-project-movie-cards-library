bf810dc989b971cc5f2e84ddef23897ab61212cf
igor <igorsife80@gmail.com>
2021-06-23 20:37:30 -0300

    cumpre o o proposito do teste MovieList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieLIst from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieLIst />
+      <MovieLIst movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,27 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <section>
-        {movies.map((filmeunico) => <MovieCard movie={ filmeunico } />)}
+        {movies.map((filmeunico) => <MovieCard key={ filmeunico.title } movie={ filmeunico } />)}
       </section>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
 
 export default MovieList;

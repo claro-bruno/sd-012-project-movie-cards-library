87664ea560bd388156eca31ecc429c1d20860eb9
ladcs <hpladcs@gmail.com>
2021-06-23 15:09:40 -0300

    Requisitos header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
     </div>
   );
 }

7ee86a2be4429b3a59a26fc7ac7cf100d7c85513
matheustkaczyk <matheus111904@hotmail.com>
2021-06-23 18:04:12 -0300

    correcoes lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropType from 'prop-types'
 
 class Rating extends React.Component {
-  render() {
-      const { rating } = this.props;
+    render() {
+    const { rating } = this.props;
     return(
-        <p>{ rating }</p>
+      <p>{ rating }</p>
     );
   }
+}
+
+Rating.propType = {
+    rating: PropType.number.isRequired,
 }
 
 export default Rating;

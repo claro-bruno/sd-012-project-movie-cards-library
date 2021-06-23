d24cdeeffa9fe3967a8fb69919c1c60913cb9adc
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-06-23 13:44:05 -0300

    Requisito 11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Rating from './components/Rating';
 import data from './data';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList movies={ data } />
+      <Rating />
     </div>
   );
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,9 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+export default class Rating extends Component {
+  render() {
+    return (
+      <p>rating</p>
+    );
+  }
+}

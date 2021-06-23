deb339aaf0bd7d01315d1816a7be0ae859adba48
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-06-23 14:46:25 -0300

    add a component Header and its text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="header">
+                <p className="header-title">Movie Cards Library</p>
+            </header>
+        )
+    };
+}
+
+export default Header;

9df2b0404b832de78f0e4f06ab6af46ea6b5108d
Eicke Yumoto <eickeyumoto@gmail.com>
2021-06-23 16:30:46 -0300

    requisito 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
 // Iniciando o projeto 🚀
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+// import React from 'react';
+// import MovieCard from './MovieCard';
+
+// class MoviesList extends React.Component {
+//   render() {
+//     const movies = this.props.movies;
+//     return {
+//       <section>
+//         {
+//           movies.map(movie => <MovieCard key={ movie.title } movie={ movie } />)
+//         }
+//       </section>
+//     }
+//   }
+// }

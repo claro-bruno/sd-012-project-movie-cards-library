db44239e3de6fb9368b78bdb29c275759cf26def
Elias Forte <eliasforte.dev@gmail.com>
2021-06-23 15:04:29 -0300

    requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,1 @@
-import React, { Component } from 'react';
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1>Movie Cards Library</h1>
-      </header>);
-  }
-}
-
-export default Header;

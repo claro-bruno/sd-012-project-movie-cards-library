1e0627e752f70854068eb530396449892c1697c6
Jossany Moura <mjossany@gmail.com>
2021-06-23 16:19:57 -0300

    Resolve requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return ('Oi');
+  }
+}
+
+export default MovieList;

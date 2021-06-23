60981bc9ff85cc49d109708398cd08920a6e91c0
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-06-23 19:58:18 -0300

    cria MovieList, renderiza MovieCard, passa key para MovieCard, cria MovieCard, renderiza image, titulo , subtitulo e sinopse dentro das tags especificadas; cria App e renderiza MovieList dentro de App

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,28 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieList;

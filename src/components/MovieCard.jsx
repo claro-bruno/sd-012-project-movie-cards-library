6f2fc78a99043e3fd118f32f16e084f1d24de3ba
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-06-23 16:15:02 -0300

    resolucao req 3,4,5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import '../App.css';
 
 class MovieCard extends React.Component {
-
+  render() {
+    return <div>aa</div>;
+  }
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,22 @@
 // implement MovieList component here
 import React from 'react';
-import moviesList from '../data';
+import PropType from 'prop-types';
 import MovieCard from './MovieCard';
 import '../App.css';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <section>
-        {moviesList.map((movie) => <MovieCard key={ movie.rating } movies={ movie } />)}
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </section>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropType.arrayOf(PropType.object).isRequired,
+};
 
 export default MovieList;

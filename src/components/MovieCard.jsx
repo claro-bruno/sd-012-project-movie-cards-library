5d425701c2fe53db7f6cf4d48bfddb8f3b3762fb
Abner <gedeaoabner@gmail.com>
2021-06-23 15:40:09 -0300

    adiciona funcionalidade de Header, MovieList e MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,31 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p>{storyline}</p>
+        <hr />
+        <p>{rating}</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard key="" movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;

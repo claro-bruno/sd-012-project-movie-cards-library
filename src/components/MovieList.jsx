17d92173af1949bb804d9ff521cea5ca5a2540a4
Patrick Dack <patrickdack89@gmail.com>
2021-06-23 15:24:02 -0300

    Requisito 3 e 16 - Foi criado o componente <MovieList /> e renderizado dentro de App

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movie-list">xablau</section>
+    );
+  }
+}
+
+export default MovieList;

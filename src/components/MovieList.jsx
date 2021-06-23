32289debf822816a09ca7d26347eb564a7ee8f31
Teika <shimworks.tk@gmail.com>
2021-06-23 15:08:25 -0300

    Completa os requisitos 01 e 02, e começa 03 e 04

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main>
+        { movies.map((elm) => <MovieCard key={ elm.title } movies={ elm } />) }
+      </main>
+    );
+  }
+}
+
+export default MovieList;

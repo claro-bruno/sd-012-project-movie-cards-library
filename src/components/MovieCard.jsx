403bf24e339b3b26f6cd6c2267cc7f82d5b25f32
Raphael <raphael.ph2ll@gmail.com>
2021-06-23 17:45:38 -0300

    Resolve ex04-06 - MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <h1>{movie.title}</h1>
+    );
+  }
+}
+MovieCard.propTypes = {
+  movie: PropTypes.exact({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+// Tive dificuldades com proTypes então consultei o repositorio do Thalles Carneiro
+// https://github.com/tryber/sd-012-project-movie-cards-library/pull/23/commits/319ca89a150ece4ab3b6550d4956b98f49e7d0c6
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,27 @@
-// implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <h1>aloha</h1>
+      <main className="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.exact({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
+// Tive dificuldades com proTypes então consultei o repositorio do Thalles Carneiro
+// https://github.com/tryber/sd-012-project-movie-cards-library/pull/23/commits/319ca89a150ece4ab3b6550d4956b98f49e7d0c6
 export default MovieList;

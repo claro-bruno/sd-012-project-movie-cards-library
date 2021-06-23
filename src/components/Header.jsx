18eb3c78267f53f88ab6b0462a04d7330b4ecf65
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-23 14:54:29 -0300

    Realiza requisitos 1 e 2 (Header)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-//Iniciando projeto
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;

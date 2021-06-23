52bee048fef0a5c11d68e18e931f848a72527309
Gabriel Viana <g.viana87@hotmail.com>
2021-06-23 16:02:52 -0300

    Item 3 - Crie um componente <MovieList />

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
         <Header />
+        <MovieList />
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <p>movieList</p>
+    );
+  }
+}
+
+export default MovieList;

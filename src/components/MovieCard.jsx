bedebb92c1d5025721063bdadc29fb9f25df44b9
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-23 18:03:56 -0300

    Requisitos 3, 4 e 5 - Cria Componente MovieList e Componente MovieCard sem conteudo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } Movie={ movie } />
+        ))}
+      </section>
+    );
+  }
+}
+
+export default MovieList;

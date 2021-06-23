85fa717321f3e43a71889223c0ffc5a95f8faef7
uriel <urielbochi@gmail.com>
2021-06-23 18:28:23 -0300

    Corrigindo erro de tag MovieCards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     return (
       <div>
-        <img src={ movie.imagePath } alt="Informações do filme"></img>
+        <img src={ movie.imagePath } alt="Informações do filme"/>
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
         <p>{ movie.storyline }</p>

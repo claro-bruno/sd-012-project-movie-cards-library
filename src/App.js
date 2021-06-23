686e64d0f4c9ce6fba91ac0816f766ea147a7164
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-23 14:32:24 -0300

    Correcao dos ultimos requisitos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
-// import movies from './data';
-// import Header from './components/Header';
-// import MovieList from './components/MovieList';
+import movies from './data';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Oi</h1>
-      {/* <Header />
-      <MovieList filmes={ movies } /> */}
+      <Header />
+      <MovieList filmes={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const [movies] = this.props;
+    const { movies } = this.props;
     return (
       <main>
         { movies.map((item) => <MovieCard key={ item.title } movies={ item } />) }
       </main>);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;

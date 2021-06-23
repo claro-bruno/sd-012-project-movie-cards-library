d5b6199381346cb62f29a28d3a67d0c2b2be38e0
uriel <urielbochi@gmail.com>
2021-06-23 18:04:53 -0300

    Checando erros do MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
-    render () {
-    const { movie } = this.props
+  render() {
+    const { movie } = this.props;
     return (
-        <div>
-            <img src={movie.imagePath}></img>
-            <h4>{movie.title}</h4>
-            <h5>{movie.subtitle}</h5>
-            <p>{movie.storyline}</p>
-            <div className="movie-card-rating"><Rating rating={ movie.rating } /></div>
+      <div>
+        <img src={movie.imagePath} alt="Informações do filme"></img>
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+        <div className="movie-card-rating"><Rating rating={ movie.rating } /></div>
         </div>
     )
     }
@@ -30,6 +30,6 @@ MovieCard.propTypes = {
   MovieCard.defaultProps = {
     movie: {},
   };
-  
+
 
 export default MovieCard

72a0263289785ca95818ff42c0636c62545849dc
Euller Braz <euller.braz99@gmail.com>
2021-06-23 15:15:48 -0300

    Requisito 3: Renderizado MovieList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <p>Teste movies</p>
+    );
+  }
+}
+
+export default MovieList;

4c6dd09b0332380a906247cbaf8e86e3a706a148
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-06-23 19:28:58 -0300

    Esqueci do resto dos commits: commitão bomba, ativar

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main className="movie-list">
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,25 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <span className="rating">
+          {rating}
+        </span>
+      </div>
+    );
+  }
+}
+
+Rating.defaultProps = {
+  rating: 1,
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+export default Rating;

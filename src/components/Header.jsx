3d919b98d8d969d36b1e9ebf0ccffe9e5f89111f
Victor <juanvictor70@gmail.com>
2021-06-23 15:26:19 -0400

    revisando linter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return <header><h1>Movie Cards Library</h1></header>
-    }
+  render() {
+    return <header><h1>Movie Cards Library</h1></header>;
+  }
 }
 
 export default Header;

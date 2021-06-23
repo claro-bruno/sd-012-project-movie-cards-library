0aeb02ac5fd7d9834d6ecc9114d090566b134e7c
pedro21medeiros <pedro21medeiros@gmail.com>
2021-06-23 19:05:06 -0300

    estiliza cores tanto do header quanto do corpo, tambem faz melhoramentos visuais

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,7 @@ class Rating extends React.Component {
     const { rating } = this.props;
     return (
       <div className="rating">
-        <p>{rating}</p>
+        <p className="nota">{rating}</p>
       </div>
     );
   }

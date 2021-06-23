ce7cc15efe9debb02abdd285eaccff6a86721df7
alestori <alestori7@gmail.com>
2021-06-23 12:22:08 -0300

    Req 17.2: PropTypes para o componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 const MovieCard = (props) => {
@@ -14,6 +15,16 @@ const MovieCard = (props) => {
       <Rating rating={ rating } />
     </li>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default MovieCard;

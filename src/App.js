6a58da1839ac7b744ab593d660d702625ad0968f
ladcs <hpladcs@gmail.com>
2021-06-23 16:25:39 -0300

    R3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }

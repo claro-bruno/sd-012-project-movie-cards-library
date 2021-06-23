6f17fd13caeb75c46eccd6f9f7a3b7544bd3cdd6
uriel <urielbochi@gmail.com>
2021-06-23 17:55:55 -0300

    Enviando erros do Header 2.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-          <h1>Movie Cards Library</h1>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
 }
 
-export default Header
+export default Header;

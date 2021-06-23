a074e9b2600e0407528f65b147c902baa2b5684e
Guilherme Cesconeto <cesconeto@gmail.com>
2021-06-23 15:47:38 -0300

    Fazendo a 1 e começo da 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h2>Hola que tal</h2>
+    )}
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      movies.map(movie => <MovieCard />)
+    )}
+}
+
+export default MovieList;

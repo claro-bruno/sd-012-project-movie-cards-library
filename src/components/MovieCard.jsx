f770efab8bf88bd280189b1694f6c10ea9d1710b
Rafael Cardoso <rafael.vimur@gmail.com>
2021-06-23 16:15:50 -0300

    ajuste no 6 e adiciona do 7 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <h1>Teste</h1>
+      <div>
+        <img src={ imagePath } alt="imagem do filme" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
     );
   }
 }
 
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
 export default MovieCard;

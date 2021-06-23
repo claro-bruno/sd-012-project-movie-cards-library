09279f1c874d7f26b4bd789799b02e5d96d8d5b2
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-23 12:17:20 -0300

    Requirement 05-> Add key for `MovieCard`

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ class MovieList extends Component {
   render() {
     const { movies } = this.props;
 
-    return movies.map((movie) => <MovieCard movie={ movie } />);
+    return movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
   }
 }
 

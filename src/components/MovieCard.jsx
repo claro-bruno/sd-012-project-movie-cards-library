b4676df0a0a37490bb9bcb496f7dcdeda813762c
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-23 17:03:52 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,7 +11,6 @@ class MovieCard extends React.Component {
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
         <p>{ movie.storyline }</p>
-        <p>{ movie.rating }</p>
       </div>
     );
   }

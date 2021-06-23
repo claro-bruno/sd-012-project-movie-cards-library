fb3574bd44e3d6c65eeebc732a08406787ede66b
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-06-23 16:00:42 -0300

    Requisito 3 - Renderiza o component MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        {/* {this.props.movies} */}
+      </section>
+    );
+  }
+}
+
+export default MovieList;

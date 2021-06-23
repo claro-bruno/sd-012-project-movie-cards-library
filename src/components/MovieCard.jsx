a748bdc4657393f2c633d738d2817a029119a807
Patrick Dack <patrickdack89@gmail.com>
2021-06-23 16:42:39 -0300

    Requisitos 11, 12 ,13 e 14 - Cria e renderiza o componente Rating com o atributo rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ movie.title }</h4>
         <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
         <p className="movie-card-storyline">{ movie.storyline }</p>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }

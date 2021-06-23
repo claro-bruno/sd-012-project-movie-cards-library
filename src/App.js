250f7134f976ef42556b3ef61b9859fac3f0da0e
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-23 14:52:10 -0300

    criando a base do componente MovieList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import movies from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }

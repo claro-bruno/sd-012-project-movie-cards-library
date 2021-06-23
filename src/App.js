67aedd421a14a18a93d1de4541046e9c5c2f03e4
uriel <urielbochi@gmail.com>
2021-06-23 17:43:08 -0300

    Corrigindo Lint APP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css'; 
+import './App.css';
 import MovieList from './components/MovieList';
 import Header from './components/Header';
 import movies from './data';
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-     <MovieList movies = { movies } />
+      <MovieList movies={ movies } />
     </div>
   );
 }

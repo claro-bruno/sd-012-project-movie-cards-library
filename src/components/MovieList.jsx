8016919974169e3e14ff7f2dffec0ea7d948a12f
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-23 15:45:35 -0300

    Resolve o requisito 3, verifica que o requisito 2 foi resolvido tambem no commit anterior

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+import data from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    data.map((movie) => <MovieCard obj={movie} />);
+  }
+}
+
+export default MovieList;

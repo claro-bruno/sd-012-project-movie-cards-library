478ae1d57a47a8c3099adcb39fdfa69b1fdd1c5c
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-23 15:52:58 -0300

    Requisitos 12 ao 14 - Rating Component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={ rating } />
       </div>);
   }
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return <div>1</div>;
+    const { rating } = this.props;
+    return <div className="rating">{ rating }</div>;
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;

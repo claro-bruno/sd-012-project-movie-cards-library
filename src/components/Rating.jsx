2b3de76b17e42b53fbbcae5393d0ab151045321b
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-23 19:11:01 -0300

    Requisitos 11, 12, 13 e 14 - Cria Component Rating e validaçao PropType

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
 // implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <p className="rating">{rating}</p>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;

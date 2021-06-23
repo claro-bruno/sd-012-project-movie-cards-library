aae8565c0ec170c80d7eb19c75a8bd6518674b47
rachidb5 <jordan.rachid@gmail.com>
2021-06-23 17:42:40 -0300

    movielist component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react'
+
+class MovieList extends React.Component {
+    render() {
+        return <div className='container'>
+            {this.props.movies}
+        </div>
+    }
+}
+
+export default MovieList

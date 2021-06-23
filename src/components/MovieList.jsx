32e54d53f3ebee996a6c4faa3c5e393c764771cb
AndZoni <lorenzoniandre@outlook.com>
2021-06-23 20:21:55 +0100

    Add Requirement 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = [];
+
+    return (<span>{movies}</span>);
+  }
+}
+
+export default MovieList;

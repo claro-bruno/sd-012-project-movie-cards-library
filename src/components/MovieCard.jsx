0684464634a0692dd0473ff126a6e7fd19d90556
Adriana Biberg <adribiberg@hotmail.com>
2021-06-23 18:51:57 -0300

    Cumpre requisitos 11, 13, 14, 15 e 16 - Cria um componente Raiting e renderiza a nota de um filme

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -12,7 +13,7 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <span>{rating}</span>
+        <Rating rating={ rating } />
       </section>);
   }
 }

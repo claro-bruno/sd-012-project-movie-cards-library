f8c5275f5a9512f84fe470f132809a4be84fc7c1
Renata Nunes <renatanunes231@yahoo.com.br>
2021-06-23 17:21:07 -0300

    componente Rating criado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-// import PropTypes from  'prop-types';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    // const { key, movie } = this.props;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <h1>Teste</h1>
+      <section>
+        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </section>
     );
   }
 }
 
-// MovieCard.propTypes = {
-//     movie: PropTypes.object.isRequired,
-//     key: PropTypes.string.isRequired,
-// };
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;

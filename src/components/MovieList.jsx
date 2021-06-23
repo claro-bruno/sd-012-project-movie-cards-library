501e9167673f0d09affa2bc8cd55617c0cb643ab
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-23 12:15:24 -0300

    Requirement 04-> Render component `MovieCard` in `MovieList`

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
+
+    return movies.map((movie) => <MovieCard movie={ movie } />);
   }
 }
 

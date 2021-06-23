be7aa67fe461b71e2a4ce9c737ca3811f1d30197
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-23 19:32:09 -0300

    Resolve requisito 4 - MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <h1>MovieCards</h1>;
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
 
-    return ({ movies });
+    return (
+      movies.map((movie) => <MovieCard key={ movie } />)
+    );
   }
 }
 

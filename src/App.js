bea578db74f917403528e37086e0097e9f564afa
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-23 16:05:47 -0300

    (2) - Cria componente Movie Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieCard from './components/MovieCard';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieCard movie={ movies[0] } />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
+
+    return (
+      <section>
+        <img src={ imagePath } alt="" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;

36db91efa0acc183714e3b98bae0648de3816a32
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-06-23 15:55:09 -0300

    Criando component <Header /> e Renderize um texto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { MovieListInfo } = this.props
+
+    return (
+      <MovieList>
+      </MovieList>
+    )
+  }
+}
+
+MovieList.propTypes = {
+  MovieListInfo: PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    rating: PropType.string,
+    imagePath: PropType.string,
+  })
+};
+
+export default MovieList;

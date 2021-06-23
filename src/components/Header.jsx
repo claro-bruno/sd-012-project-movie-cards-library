8bb9713f492f352b7679a470b2d663bb2a1a05d5
RyanLaiber <ryanlaiber@gmail.com>
2021-06-23 15:58:07 -0300

    corrige linter do Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 
 class Header extends React.Component {
   render() {
-    <header>
-      <h1>Movie Cards Library</h1>
-    </header>
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
   }
 }
 

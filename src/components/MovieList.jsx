a73833f1a86e22bd049e182cca9816593b171617
Victor <juanvictor70@gmail.com>
2021-06-23 17:08:20 -0400

    renderizando MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return <div><p>Movie List</p></div>;
+  }
+}
+
+export default MovieList;

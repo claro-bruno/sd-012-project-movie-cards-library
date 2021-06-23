12c128710bcf064f13b94c68c9d4cabd3b32980d
Gabriel Takaki <gabitj@icloud.com>
2021-06-23 18:02:37 -0300

    Corrigindo ultimos dois requisitos

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import MovieList from './components/MovieList';
 import data from './data';
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <MovieList movies={ data } />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieList movies={ data } />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;

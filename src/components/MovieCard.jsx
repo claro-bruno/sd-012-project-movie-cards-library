eacd1f0936b559903f3f59b2651cb8bc92a83b61
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-06-23 15:57:34 -0300

    Renderiza sinopse do filme no componente MovieCard - Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,6 +12,7 @@ class MovieCard extends React.Component {
         <img src={ movie.imagePath } alt={ movie.title } />
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
       </div>
     );
   }

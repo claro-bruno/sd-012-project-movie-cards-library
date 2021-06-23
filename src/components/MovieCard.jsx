2725f096f663b057009785771c63d131d2336954
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-06-23 19:05:43 -0300

    feita a aba de rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={ imagePath } alt="vish" />
+        <img className="movie-card-image" src={ imagePath } alt="vish, no pics allowed" />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
+        <Rating rating={ rating } key="rating" />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,22 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        Rating:
+        <span className="rating">
+          { rating }
+        </span>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;

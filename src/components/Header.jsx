619bccb2706c047078769c085239f04adc616065
CamilaDamasio <camiladamasioef@hotmail.com>
2021-06-23 14:53:11 -0300

    Corrige erro lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-// implement Header component here
 import React from 'react';
-class Header extends React.Component {
-    render() {
-      return (<Header></Header>);
-    }
-  }
 
-  export default Header;
+class Header extends React.Component {
+  render() {
+    return (<Header>Hello world!</Header>);
+  }
+}
+
+export default Header;

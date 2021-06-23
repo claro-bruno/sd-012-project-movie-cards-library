ae2734705bce2eb8ff4c76803f065447aefc3dfb
Eurides <euridesalves@yahoo.com.br>
2021-06-23 17:00:53 -0300

    Criado Componente Header, renderizado texto no Header, criado componten MovieList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import MovieList from './MovieList';
+import MovieCard from './MovieCard';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
+      <MovieList />
+      <MovieCard />
     </div>
   );
 }
---
+++ src/MovieList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        <p> </p>
+      </section>
+    );
+  }
+}
+
+export default MovieList;

534a97afc3ecd2e4361cff1f066b1ec7a303f581
Guilherme Dias <gmd.delta@gmail.com>
2021-06-23 19:55:12 -0300

    [PT-BR] Corrigindo Lint. [EN] Fixing Lint.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
-import { shape, string, number} from 'prop-types';
+import { shape, string, number } from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {

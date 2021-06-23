4edfcb37c7f9e59ff1923654a82fc3452f3e681f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 15:49:27 -0300

    requisito 5 - Passe uma key para cada `<MovieCard />` correcao passando a key correta

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+        <span>{title}</span>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.object.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,15 @@
 // implement MovieList component here
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
+    console.log(movies);
     return (
       <div>
-        {movies.map((elem, index) => <MovieCard key={ index } movie={ elem } />)}
+        {movies.map((elem) => <MovieCard key={ elem.title } movie={ elem } />)}
       </div>
     );
   }

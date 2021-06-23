c11f1f85a499e2a76eef41d80721107441712922
Gabriel Takaki <gabitj@icloud.com>
2021-06-23 16:31:58 -0300

    Requisito 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,36 @@
 // implement MovieCard component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import '../App.css';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        <div className="movie-card">
+          <img className="movie-card-image" src={ movies.imagePath } alt="" />
+          <main className="movie-card-body">
+            <h2 className="movie-card-title">{movies.title}</h2>
+            <p className="movie-card-subtitle">{movies.subtitle}</p>
+            <p className="movie-card-storyline">{movies.storyline}</p>
+            <div className="movie-card-rating">
+              <p>Rating</p>
+              <p className="rating">{movies.rating}</p>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.defaultProps = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  imagePath: PropTypes.string,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+import '../App.css';
+import info from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      info.map((movie) => <MovieCard key={ movie.title } movies={ movie } />)
+    );
+  }
+}
+
+export default MovieList;

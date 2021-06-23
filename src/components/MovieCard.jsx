e62968d2de4ea71ed44b594985a6b92315d334f5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 16:50:30 -0300

    requisito 13 - Renderize o componente `<Rating />` dentro de `<MovieCard />`

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,19 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ `Banner for ${title}` } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={ rating } />
       </div>
     );
   }

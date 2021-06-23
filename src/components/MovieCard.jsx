ba0bddf94ef932cc93b63a8608c60328813ebd48
Victor <juanvictor70@gmail.com>
2021-06-23 17:37:57 -0400

    renderizando MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+	render() {
+		return <div><p>MovieCard</p></div>
+	}
+}
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+    render() {
+        return <div><p>Rating</p></div>;
+    }
+}
+
+export default Rating;

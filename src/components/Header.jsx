71eca5b8d3137e3c1f52f300b86b2a3aef3c7aea
Gabriel Takaki <gabitj@icloud.com>
2021-06-23 15:00:42 -0300

    Requisito 1, adiciona um componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
 // implement Header component here
+import React from 'react';
+import '../App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className='movie-card-header'>
+        <h1 className='page-title'>Movie Cards</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;

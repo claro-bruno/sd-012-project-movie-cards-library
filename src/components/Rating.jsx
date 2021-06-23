2b8813d5e45d1b1a459ea89cbab1f39a50d4bce1
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-23 19:16:14 -0300

    Implementaçao de componente Rating (requisitos 11 e 12)

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,23 @@
 // implement Rating component here
+import React, { Component } from 'react';
+import '../App.css';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="rating">{rating}</div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
+
+export default Rating;

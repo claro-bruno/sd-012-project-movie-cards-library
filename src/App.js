1e8897c085a8d968f4b4730004feec939e69efbe
Wellington <junior.wel02@exemplo.br>
2021-06-23 14:48:40 -0300

    requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Header />
     </div>
   );
 }

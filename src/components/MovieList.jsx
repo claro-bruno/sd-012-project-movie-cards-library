59eb21e0de3c085695e573592603145215421379
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-06-23 17:22:45 -0300

    Resolve erros de lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -16,7 +16,7 @@ class MovieList extends React.Component {
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object),
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;

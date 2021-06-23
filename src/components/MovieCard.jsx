519883ef9e5fd8e8026df58b8b781202548985e2
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-06-23 13:38:34 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 export default class MovieCard extends Component {
   render() {
-    const { movie: { imagePath, title, subtitle } } = this.props;
+    const { movie: { imagePath, title, subtitle, storyline } } = this.props;
     return (
       <section>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </section>
     );
   }
@@ -19,5 +20,6 @@ MovieCard.propTypes = {
     imagePath: PropTypes.string,
     title: PropTypes.string,
     subtitle: PropTypes.string,
+    storyline: PropTypes.string,
   }).isRequired,
 };

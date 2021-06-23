7c78f581ca9ef844d71e9f8656090ab51cca7918
Patrick Dack <patrickdack89@gmail.com>
2021-06-23 15:59:53 -0300

    Requisito 8 - Renderiza o titulo dentro de uma tag h4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,19 +7,20 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{ movie.title }</h4>
       </div>
     );
   }
 }
 
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    imagePath: PropTypes.string,
-    title: PropTypes.string,
-    subTitle: PropTypes.string,
-    rating: PropTypes.number,
-    storyLine: PropTypes.string,
-  })
-}
+// MovieCard.propTypes = {
+//   movie: PropTypes.shape({
+//     imagePath: PropTypes.string,
+//     title: PropTypes.string,
+//     subTitle: PropTypes.string,
+//     rating: PropTypes.number,
+//     storyLine: PropTypes.string,
+//   })
+// }
 
 export default MovieCard;

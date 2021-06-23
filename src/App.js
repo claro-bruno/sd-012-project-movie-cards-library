68339a8c52d856803adf95d7927abb9896880212
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-06-23 14:35:18 -0300

    Abrindo PR

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
+    </div> 
   );
 }
 

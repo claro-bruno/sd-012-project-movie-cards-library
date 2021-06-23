7c5e1d60cdec0db3b992187140f91f3b5fae54d5
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-23 15:40:15 -0300

    Rendezirado uma nota de um filme n o componente Rating - Requisito 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      
-    )
+      <p>
+        Rating
+        { rating }
+      </p>
+    );
   }
 }
+
+export default Rating;

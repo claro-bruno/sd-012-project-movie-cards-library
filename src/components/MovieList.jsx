56127201334ae9d04a0110a786963c311eead56d
alestori <alestori7@gmail.com>
2021-06-23 12:02:22 -0300

    Reqs 3 & 4: cria componente MovieList e renderiza MovieCard nele"

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+const MovieList = (props) => {
+  const { movies } = props;
+  return (
+    <ul>
+      { movies.map((movie) => <MovieCard key={ movie.title } alt={ movie.title } />) }
+    </ul>
+  );
+};
+
+export default MovieList;

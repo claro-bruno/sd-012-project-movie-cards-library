a829f7e6402760c0ad00ea36d4f3841cf45be49f
André Dantas <aluizcfd@gmail.com>
2021-06-23 18:44:49 -0300

    Adição do component Rating.jsx e seu conteúdo, renderização do mesmo componente feita em MovieCard

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+
+    return (
+      <div>
+        <p className="rating">{ rating }</p>
+      </div>
+    );
+  }
+}
+
+export default Rating;

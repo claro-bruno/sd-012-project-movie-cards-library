e6a57a68c4a98e6815baf216aeeba280ac49dfbd
RenanMiyashita <renanmiyashita@gmail.com>
2021-06-23 17:25:09 -0300

    requisitos 3, 4 e 5 completos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <h1>Xablau</h1>
+  }
+}
+
+export default MovieCard;

8055e8ed874df917002a35153f8b08e2428281ef
CamilaDamasio <camiladamasioef@hotmail.com>
2021-06-23 16:37:48 -0300

    Verifica requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <MovieList movieList={ movies[0].title } />
+      </div>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Movies extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies[0].title }
+      </div>);
+  }
+}
+
+Movies.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default Movies;

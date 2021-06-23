9996c6c3ef72f94f8e1c7afd4336fe66f9a9f7c1
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-23 15:20:32 -0300

    Criado o componente MovieList - Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default MovieList;

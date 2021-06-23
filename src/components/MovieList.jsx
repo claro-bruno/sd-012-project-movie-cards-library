319ca89a150ece4ab3b6550d4956b98f49e7d0c6
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-23 15:41:45 -0300

    Requisitos 4 e 5
    4 - Renderiza componentes <MovieCard /> dentro de <MovieList />
    5 - Passa uma key para cada <MovieCard /> renderizado com o nome do filme

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return ();
+    return (movies.map((movie) => <MovieCard key={ movie.title } />));
   }
 }
 

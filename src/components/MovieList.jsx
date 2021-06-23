514bc0687d3b8a41db75aefc3bd19317ba929836
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-23 14:59:27 -0300

    adicionando o componente MovieCard em MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const movies = this.props.movies;
     return (
-      <h1></h1>
+      <div>
+      {movies.map((movie) => <MovieCard movie={movie}/>)}
+      </div>
     );
   }
 }

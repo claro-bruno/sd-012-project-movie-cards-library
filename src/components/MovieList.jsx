7b7bf0b9a6e294bf440c3dbeda19e657a995309b
walttoncoelho <walltton@outlook.com>
2021-06-23 15:34:19 -0300

    Movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+class MovieList extends React.Component {
+    render() {
+        const { movies } = this.props;
+
+        return (
+            <div className="movie-list">
+                {
+                    movies.map((movie, index) => (
+                        <MovieCard key={movies[index].title} movie={movie} />
+                    ))
+                }
+            </div>
+        );
+
+    }
+}
+export default MovieList;

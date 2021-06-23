b44458eedc6b0a34ef53b92d898b7803c18459e5
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-06-23 14:57:54 -0300

    Requisitos 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <li>{ movie.title }</li>
+    );
+  }
+}
+
+MovieCard.propTypes = PropTypes.shape({
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  imagePath: PropTypes.string,
+}).isRequired;
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        <ul>
+          { movies.map((movie, index) => <MovieCard key={ index } movie={ movie } />) }
+        </ul>
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = PropTypes.array.isRequired;
+
+export default MovieList;

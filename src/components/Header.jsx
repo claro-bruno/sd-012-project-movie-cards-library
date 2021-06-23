7f1b25a28f503da765d67ed441747646fd4c307b
jmlinger <jmlinger10@gmail.com>
2021-06-23 15:44:13 -0300

    resolve requisito 1: adicionou-se um texto em Header e o renderizou

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
  class Header extends Component {
   render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
   }
 }
 

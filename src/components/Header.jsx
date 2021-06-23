3c259f1db33e6c00527b2a1ed8bab03f3fa7afcf
Wellington <junior.wel02@exemplo.br>
2021-06-23 15:04:03 -0300

    requisito 2 correçao retirada de espaço

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1> Movie Cards Library </h1>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main>a</main>
+    );
+  }
+}
+
+export default MovieList;

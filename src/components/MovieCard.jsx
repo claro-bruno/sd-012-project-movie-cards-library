35041dd564df741bc9cd29539dd8e156a197db4d
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-23 17:13:22 -0300

    adiciona imagens dos filmes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,25 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title } = movie;
-    return <div>{ title }</div>;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <div>
+        { title }
+        { subtitle }
+        { storyline }
+        { rating }
+        <img src={ imagePath } alt={ title } />
+      </div>
+    );
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.shape({ title: PropTypes.string }).isRequired,
+  movie: PropTypes.shape({ title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.string }).isRequired,
 };
 
 export default MovieCard;

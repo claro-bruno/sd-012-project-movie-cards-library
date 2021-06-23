5700da464818686db2c1a289495911901cd804ff
Willian Prado <willian_prado@hotmail.com>
2021-06-23 17:06:19 -0300

    Resolve os requisitos 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,6 +12,12 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">
             { title }
           </h4>
+          <h5 className="movie-card-subtitle">
+            { subtitle }
+          </h5>
+          <p className="movie-card-storyline">
+            { storyline }
+          </p>
         </div>
       </div>
     );

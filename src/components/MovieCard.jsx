def1cdf31b6f2d7ed14f8a501f86f63b390ea875
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-23 12:25:43 -0300

    Requirement 07-> Add element `img` in the `MovieCard`

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,5 +3,13 @@ import React, { Component } from 'react';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
+
+    return (
+      <div>
+        <img src={ movie.imagePath } alt={ movie.title } />
+      </div>
+    );
   }
 }
+
+export default MovieCard;

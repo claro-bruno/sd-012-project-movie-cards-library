8f7487c63dffb39b0823468ecbaa91fec04029a6
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 16:27:47 -0300

    Adiciona o <Header /> no App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import data from './data';
 import MovieList from './components/MovieList';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList movies={ data } />
     </div>
   );

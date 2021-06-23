00d79179dc47a0bbcfff33c094c1f1e05be06964
Victor Emmanuel <victorpassos2012@gmail.com>
2021-06-23 17:50:58 -0300

    Requisito 6 feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class movieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card movie-card-body" />
+    );
+  }
 }
 
 export default movieCard;

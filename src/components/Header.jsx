c7e324a8a01a16ba66a26462c16a3a64975ffca3
rachidb5 <jordan.rachid@gmail.com>
2021-06-23 16:27:13 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,14 @@
 // implement Header component here
-import React from 'react'
+import React from 'react';
 
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <h1 className="navbar-brand">Movie Cards Library</h1>
+      </nav>
+    );
+  }
+}
+
+export default Header;

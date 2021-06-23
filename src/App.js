24dec18a803ec94082b7d8f99c7d2a98c1bf745e
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-23 16:55:11 -0300

    Adicionado props para o componente MovieList; Renderizado no APP o Movielist chamando os filmes do arquivo data.js; adicionado proptype ao MovieList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,19 @@
 // implement MovieList component here
 import React from 'react';
-import movies from '../data';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       movies.map((filme) => <MovieCard key={ filme.title } movie={ filme } />)
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;

a0b99a867c152a286d9ce013f39dc896661c96f2
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-23 18:48:29 -0300

    Requisitos 6, 7, 8, 9 e 10 - Adiciona validaçao de PropTypes e correçoes para o Lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,31 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={movie.imagePath} alt="" />
+        <img className="movie-card-image" src={ movie.imagePath } alt="" />
         <div className="movie-card-body">
-          <h4 className="movie-card-title">{movie.title}</h4>
-          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
-          <p className="movie-card-storyline">{movie.storyline}</p>
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
         </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;

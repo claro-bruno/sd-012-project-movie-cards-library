beade7ff9146bd7b85c624f58dad2aa6933cfec8
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-23 12:05:26 -0300

    Requirement 02-> Render header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
   }
 }
 

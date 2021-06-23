b22964030a4700e350d04fd86619bbdba65c388e
André Dantas <aluizcfd@gmail.com>
2021-06-23 18:18:32 -0300

    Correção da prop 'key' na renderização de MovieCard em MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
   render() {
     return (
       <div>
-        {movies.map((movie) => <MovieCard key={ movie.index } movie={ movie } />)}
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

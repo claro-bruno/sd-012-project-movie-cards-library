5705e54d8f622d0a9592a2aada67d4895e115eb6
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-23 17:23:24 -0300

    Resolve requisito 2(ajustes no lint) - text Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,9 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>Movie Cards Library</h1>;
+        <h1>Movie Cards Library</h1>
       </header>
-    ) 
-  };
+  )};
 }
 
 export default Header;

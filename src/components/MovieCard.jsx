25fe44abddc354862b4067a55dff23f87125c8a9
Gabriel Viana <g.viana87@hotmail.com>
2021-06-23 20:29:05 -0300

    Rating renderizando total

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
@@ -12,6 +13,7 @@ class MovieCard extends Component {
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline }</p>
+        <Rating rating={ movie.rating } />
       </section>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <section className="rating">
+        <p>{rating}</p>
+      </section>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;

94e20b86a8ba92da2a35864ccc1c0ee8ac7698f4
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-06-23 19:30:51 -0300

    Corrgindo some mistakes relacionados ao ESLint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-
     const { rating } = this.props;
 
-    return(
+    return (
       <section className="rating">
         { rating }
       </section>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;

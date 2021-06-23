f4a4805e70d3cd1767776cba8b0af7f7dda2115c
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-06-23 16:19:47 -0300

    ajustando estilo do rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,11 +6,11 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+      <div className="movie-card movie-card-body">
+        <img className="movie-card-image " src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
         <Rating rating={ rating } />
       </div>
     );

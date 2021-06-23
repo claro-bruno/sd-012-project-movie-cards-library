43209b76a67e4eeb0d183b14e0b245c69e009536
Abner <gedeaoabner@gmail.com>
2021-06-23 17:36:35 -0300

    adiciona funcionalidade de Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -8,11 +9,12 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p>{ storyline }</p>
-        <hr />
-        <section>{rating}</section>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p>{ storyline }</p>
+        </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -23,7 +25,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.string,
+    rating: PropTypes.number,
     imagePath: PropTypes.string,
   }).isRequired,
 };

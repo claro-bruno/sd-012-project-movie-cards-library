d225df21f196e9655e77d182a66b991dd0af5e75
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-23 16:47:50 -0300

    Realiza requisito 3 (MovieList) completo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-
-    const { movie } = this.props; 
-
+    const { movie } = this.props;
     return (
       <div>
         <h2>{movie.title}</h2>
       </div>
-    )
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;

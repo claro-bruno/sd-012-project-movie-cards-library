479e57c2f79be8d953d426e06eb2cf8be581a9de
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-23 15:31:53 -0300

    Realiza requisito 3 (MovieList) sem o propsType

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+
+    const { movie } = this.props; 
+
+    return (
+      <div>
+        <h2>{movie.title}</h2>
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
+      </section>
+    );
+  }
+}
+
+export default MovieList;

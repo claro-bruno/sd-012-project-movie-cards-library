2740ec22c51312462af84ceb0bafd4d1c82b0605
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 15:23:33 -0300

    requisito 4 - 4 - Renderize componentes `<MovieCard />` dentro de `<MovieList />`

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,17 @@
 // implement MovieList component here
 import React from 'react';
+// import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <section />;
+    // const { movies } = this.props;
+    return <MovieCard />;
   }
 }
+
+// MovieList.propTypes = {
+//   movies: PropTypes.array.isRequired,
+// };
 
 export default MovieList;

ec174e9c85597d4c336d42036a7fc7aeb7d289a2
Sass <dudu_sass12@hotmail.com>
2021-06-23 17:34:19 -0300

    colocando os exercicios do moviecard

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList'
-import MovieCard from './components/MovieCard'
 import movies from './data'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieCard />
       <MovieList movies ={ movies } />
     </div>
   );
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import PropType from 'prop-types';
-import MovieList from './MovieList'
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import '../App.css';
+import Rating from './Rating';
 
 
 class MovieCard extends React.Component {
   render() {
-    const { movies } = this.props
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movies
+    const { movies } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movies;
     return(
-      <div>
-        <img src={movies.imagePath} alt={movies.title}/>
-        <h4>{movies.title}</h4>
-      </div>
+      <li classname="movie-card">
+        <img classname="movie-card-image" src={ imagePath } alt={ title }/>
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 classaname="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
+      </li>
     )
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;// implement MovieCard component here

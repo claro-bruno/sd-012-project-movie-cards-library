a34b70d99082451b10c8c6fef74a2b8c12a205e4
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-23 15:29:39 -0300

    Adicionada funçao onde o componente Movielist renderiza o componente Moviecard para cada filme no arquivo data.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieCard />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 // implement MovieList component here
 import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     return (
-
+      movies.map((filme, index) => <MovieCard key={ index } movie={ filme } />)
     );
   }
 }

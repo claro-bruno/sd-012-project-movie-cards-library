a42fb5d937bc05be2ad35997e6d6587e6389eea3
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-06-23 16:05:40 -0300

    Adiciona requisitos restantes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,8 +18,8 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{ movie.title }</h4>
           <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
           <p className="movie-card-storyline">{ movie.storyline }</p>
+          <Rating rating={ movie.rating } />
         </div>
-        <Rating rating={ movie.rating } />
       </div>
     );
   }

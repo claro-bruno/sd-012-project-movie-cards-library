2a6adc0a082b170f3ac2ebe29f7e203a6451cd16
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-06-23 15:13:03 -0300

    Adiciona componente Rating - Requisito 11, 12 , 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <Rating rating="" />
+    );
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,21 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
+
     return (
-      
+      <p>
+        {rating}
+      </p>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;

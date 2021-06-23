0f9897ce55429047ce7136dfed5fc69c6238168d
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-06-23 19:13:49 -0300

    Renderizando componente <MovieCard /> in <MovieList />, usando movies e no props para passar a key, criar componente {title, subtitle, storyline, imagePath e rating} com destructuring, renderizar img, criar Rating

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render(){
+    const { movieCardInfo } =  this.props;
+    const { movie } = this.props;
+    const {title, subtitle, storyline, imagePath, rating} = movie;
+    return (
+      <section>
+        <h2>{movie.title}</h2>
+        <h3>{movie.subtitle}</h3>
+        <img src={movie.imagePath} alt={movie.title}/>
+        <h4>{movie.storyline}</h4>
+      </section>
+    )
+  }
+}
+
+MovieCard.propTypes = {
+    movieCardInfo: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  })
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <section>
-        <MovieCard />
+        {
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
       </section>
     );
   }

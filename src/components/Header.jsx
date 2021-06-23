4347452b2ef21e242474b70c2bdda43f9ee9c254
Armando <a.guerra58@hotmail.com>
2021-06-23 14:47:18 -0300

    requisitos 1 e 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+export default Header;

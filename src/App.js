ea8b4ffc1c25d5c3cdf7280a696ece8549f1fef4
Rafael Cardoso <rafael.vimur@gmail.com>
2021-06-23 17:35:24 -0300

    colando css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import list from './data';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <MovieList movies={ list } />
     </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,13 +7,15 @@ class MovieCard extends Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <div>
-        <img src={ imagePath } alt="imagem do filme" />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
-      </div>
+      <section className="movie-card">
+        <section className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt="imagem do filme" />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
+        </section>
+      </section>
     );
   }
 }

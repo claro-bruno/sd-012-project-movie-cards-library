a9ccbbc91428cc0315cb187710fb3c8970e8e31f
Eric Faria <ericdeofaria@gmail.com>
2021-06-23 16:15:22 -0300

    Requisitos 11 e 12

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movieInfo } = this.props;
     return (
       <section>
+        <img src={ movieInfo.imagePath } />
         <h4>{movieInfo.title}</h4>
         <h5>{movieInfo.subtitle}</h5>
         <p>{movieInfo.storyline}</p>
+        <Rating rating={ movieInfo.rating } />
       </section>
     );
   }
@@ -18,6 +21,8 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,18 @@
-// implement Rating component here
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <span className="rating">{ rating }</span>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+export default Rating;

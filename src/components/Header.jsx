3511e15dc53a39aa9e4e73f8bd20f4dea08023ae
paulomichael <paulo.michael@gmail.com>
2021-06-23 15:25:39 -0300

    requisito 2 - renderiza um texto no <Header />

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (<header>Heyo!</header>);
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
   }
 }
 export default Header;

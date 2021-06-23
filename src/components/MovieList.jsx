b361c5e7e009ee54cd12a624bd7eba30ab7583f4
paulomichael <paulo.michael@gmail.com>
2021-06-23 15:34:26 -0300

    requisito 3 - cria um componente <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <p>{this.props}</p>
+    );
+  }
+}
+export default MovieList;

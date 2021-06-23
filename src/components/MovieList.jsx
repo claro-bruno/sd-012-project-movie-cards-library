e0129364f4a4cb4ba7133f60d85fa18c40475ed2
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-23 15:06:08 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <p>oi</p>
+    );
+  }
+}
+
+export default MovieList;

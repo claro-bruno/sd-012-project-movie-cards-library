9673d9e4467ea51734adb3e8397efa1892d734cb
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-06-23 16:31:14 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      {}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
+      <Header/>
+      
+      </div>
   );
 }
 

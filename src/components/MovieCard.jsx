064e62539919143b447539bb3d2f814d203b8b1b
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-23 18:18:24 -0300

    Requisito 3, 4 e 5 - Cria validaçao com PropTypes e ajuste na formataçao para o Lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    return <div></div>;
+    return <div />;
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -14,5 +15,17 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      ImagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;

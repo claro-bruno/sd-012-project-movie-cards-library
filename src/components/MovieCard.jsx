bb193b76a369ebb09f67cced179d6991a00a7e2f
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-06-23 15:34:15 -0300

    Adiciona componente MovieCard - Requisito 6 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,38 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
-class MovieList extends React.Component {
+class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+
     return (
-      <div>
-        <Rating rating="" />
+      <div className="movie-card">
+        <img
+          className="movie-card-image"
+          src={ movie.imagePath }
+          alt="Imagem do Filme"
+        />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
 }
 
-export default MovieList;
+MovieCard.propTypes = {
+  movie: PropTypes.exact({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;

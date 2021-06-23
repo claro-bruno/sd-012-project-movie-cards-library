e6d666e2827c9b60414b792995f568fbbdf9b415
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-23 18:07:00 -0300

    adiciona rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,7 +11,7 @@ class MovieCard extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        { rating }
+        <Rating rating={ rating } />
         <img src={ imagePath } alt={ title } />
       </div>
     );

488ea7d75fe651c3aaf46cd3b0aef15227e467b1
Adriana Biberg <adribiberg@hotmail.com>
2021-06-23 16:47:09 -0300

    Cumpre requisito 3 - Renderiza MoviesList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MoviesList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MoviesList movies="a" />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+
+class MoviesList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return <section>{movies}</section>;
+  }
+}
+
+export default MoviesList;

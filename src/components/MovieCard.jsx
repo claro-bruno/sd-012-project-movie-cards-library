f4996ea03d6fb93647b95f1b986f75e067718e86
Euller Braz <euller.braz99@gmail.com>
2021-06-23 16:11:08 -0300

    Requisito 10: Adicionado Sinopse em cada MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath } = movie;
 
     return (
       <section>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </section>
     );
   }

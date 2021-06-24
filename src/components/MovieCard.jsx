bfd3d6fc57f731088a137c34dfd7b231e5e5eeee
Çuiz Henrique <luishenriqueee2010@hotmail.com>
2021-06-24 00:44:31 -0300

    Adiciona exercicios 6, 7, 8, 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return <h3>Random film</h3>;
+    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline } = movie;
+    return (
+      <section>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </section>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;

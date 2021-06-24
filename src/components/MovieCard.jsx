15dc8c74f954e2267a5b2da65e74fbe4d8befd73
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-06-24 09:21:34 -0300

    finaliza projeto- renderiza rating, corrige storyline em 'p' e corrige lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -8,11 +9,10 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <p>{ rating }</p>
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <hr />
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }

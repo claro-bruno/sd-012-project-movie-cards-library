579af219e4971d4f80df98ce759b4f400708391d
Eduardo Linhares <modsgtasagame@gmail.com>
2021-06-24 17:21:13 -0300

    Resolve requisitos restantes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+	render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <section className="movie-card">
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
+        <img className="movie-card-image" src={ imagePath } alt={ title }></img>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+	render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        {
+          movies.map(movie => <MovieCard key={ movie.title } movie={ movie }/>)
+        }
+      </section>
+    )
+  }
+}
+
+export default MovieList;

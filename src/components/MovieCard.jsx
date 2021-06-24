ddf7e7c2f71eb3314285f3dd1488914fad48956a
Dayvid Ruan <ruan12bbg@gmail.com>
2021-06-24 18:51:28 -0300

    Requisitos de 11 a 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class MovieCard extends React.Component {
         <h4>{ movie.title }</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
+        <Rating rating={ movie.rating } />
       </section>
     );
   }
@@ -20,6 +22,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
+    rating: PropTypes.number,
     imagePath: PropTypes.string,
   }).isRequired,
 };

ffb5b85cb0aca9eae74689de954cb525da2622a6
Milton Castro <castro.milton07@gmail.com>
2021-06-23 22:43:07 -0300

    Requisitos 12, 13 e 14 - Renderiza a nota, o componente e passa como prop

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -17,6 +18,7 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <div />
+      <span className="rating">{rating}</span>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;

d9838b6d2265c3748194b63c2cbe9d73b6c38146
Milton Castro <castro.milton07@gmail.com>
2021-06-23 22:34:08 -0300

    Requisito 11 - Cria um componente <Rating />

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Rating;

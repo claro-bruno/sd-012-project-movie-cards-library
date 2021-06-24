ea62c21b525ac19c56cc56a464418775ba491a0c
thales <thalesdaiel2013@gmail.com>
2021-06-23 21:00:55 -0300

    feito etapa 3, ainda necessario corrigir o lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.filmes.imagePath} alt={this.props.filmes.title} />
+      </div>
+    );
+  }
+}
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    const {title, subtitle, imagePath } = movies;
+    return (
+      <main>
+        <MovieCard filmes={ imagePath } />
+      </main>);
+  }
+}
+
+export default MovieList;

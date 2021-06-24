9eb1b66693e84e32321e0e7c07afb30dfe5f4150
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-24 00:29:03 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section> </section>
+    );
+  }
+}
+
+export default MovieList;

9c35b191fedfa52f6d51d3a0dc5d3422e3f80cbe
Çuiz Henrique <luishenriqueee2010@hotmail.com>
2021-06-24 01:24:18 -0300

    Adiciona requisitos 11, 12, 13, 14, 15, 16 e 17

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,21 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes, { number } from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <section className="rating">{rating}</section>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 8,
+};
+
+export default Rating;

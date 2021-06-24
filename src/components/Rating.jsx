9ec8196c5e22d1c1de82defafbf1c3576b0921a7
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-24 11:16:35 -0300

    Resolve requisito 13 - Moviecard Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return <span>Rating</span>;
+    const { rating } = this.props;
+
+    return <span className="rating">{rating}</span>;
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;

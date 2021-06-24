9796ceff6dfd57460e0a1d654edf7535c98eec2f
Ed Santos <edsantos2822@gmail.com>
2021-06-24 00:50:44 -0300

    Finalizando - Requisito 3 - Renderizando Component MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main>
+        <span>Verifica funcionamento</span>
+      </main>
+    );
+  }
+}
+
+export default MovieList;

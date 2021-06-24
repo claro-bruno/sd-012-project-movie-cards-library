59c680c525403f4525acc3b297b8e2d7b6da5357
Milton Castro <castro.milton07@gmail.com>
2021-06-23 23:52:04 -0300

    Modifica o acesso de uma prop atraves da desconstrucao e declara o PropType

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = Object.values(this.props)[0];
+    const { list } = this.props;
+    const movies = list;
     return (
       <div className="movie-list">
-        {(movies).map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;

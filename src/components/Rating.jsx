38dbfc7c2d516364feba5f40bf0f382b1c6a14e9
MaxAlves0035 <maxalves2@hotmail.com>
2021-06-24 15:40:45 -0300

    resolve erro do proptype de ranting undefined + projeto concluido

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -12,8 +12,12 @@ class Rating extends React.Component {
   }
 }
 
+Rating.defaultProps = {
+  rating: 4,
+};
+
 Rating.propTypes = {
-  rating: PropTypes.number.isRequired,
+  rating: PropTypes.number,
 };
 
 export default Rating;

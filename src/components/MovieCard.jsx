86ea288751ef9713db71a7ca59265348fa0019c4
Milton Castro <castro.milton07@gmail.com>
2021-06-23 22:20:38 -0300

    Requisitos 6 e 7 - Cria o componente <MovieCard /> e renderiza a imagem do filme

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return <div />;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+
+    console.log(`${title}
+    ${subtitle}
+    ${storyline}
+    ${rating}
+    ${imagePath}`);
+    return (
+      <img src={ imagePath } alt="" />
+      // <span>{this.props.title}</span>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;

775cce5052878ee88d12ff468baf9d08d70ce2aa
Gustavo-Mourao <gcmbh1@gmail.com>
2021-06-25 17:09:03 -0300

    Q 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 // implement Header component here
 import React from 'react';
 
-class Header extends React.component {
+class Header extends React.Component {
   render() {
     return (
-      <Header>
+      <header>
         <h1>Movie Cards Library</h1>
-      </Header>
+      </header>
     );
   }
 }

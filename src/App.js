53cb0fb7468d55e5adda61757962f3ae72b9e46e
David Azevedo <zevid.dev@outlook.com>
2021-06-25 19:08:39 -0300

    Render MovieList and import movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import data from './data';
+import movies from './data';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
+      <MovieList movies={movies}/>
     </div>
   );
 }

d3914f03c714f9d9c9d351427d51cadde0a88a0a
David Azevedo <zevid.dev@outlook.com>
2021-06-25 18:49:00 -0300

    Done requirement 3 - Create/Render movieList component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return(
+      <main className="movie-list">
+        <p>movieList</p>
+      </main>
+    )
+  }
+}
+
+export default MovieList;

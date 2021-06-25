f34425b6901c17af613addc3c5951fae310a5e89
Kevin Willian <kevinwillian084@gmail.com>
2021-06-24 23:02:03 -0300

    Adiciona requisito 4, 5 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+        <p>Sou um movie</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-
+import MovieCard from './MovieCard';
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } />
+        ))}
       </div>
     );
   }

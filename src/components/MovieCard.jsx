95b6bcf3e4400c0650c87a6d93531401483ecc31
Victor <juanvictor70@gmail.com>
2021-06-25 11:42:36 -0400

    finalizando layout

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,14 +8,15 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     return (
       <div className="movie-card">
-        <img src={ movie.imagePath } alt={ movie.title } />
-        <div>
-          <h4>{ movie.title }</h4>
-          <h5>{ movie.subtitle }</h5>
-          <p>{ movie.storyline }</p>
-          {/* <p>{ movies.rating }</p> */}
+        <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
         </div>
-        <Rating rating={ movie.rating } />
+        <div className="movie-card-rating">
+          <Rating className="rating" rating={ movie.rating } />
+        </div>
       </div>
     );
   }

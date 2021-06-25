2ae174ff10e05f4376744565fcc87dfc52668def
Kevin Willian <kevinwillian084@gmail.com>
2021-06-24 23:12:59 -0300

    Adiciona requisitos 10 e 11

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,6 +9,7 @@ class MovieCard extends Component {
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Rating;

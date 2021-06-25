5d834d8e3b2af2a8a2b3b6369f1c22a5257603d1
David Azevedo <zevid.dev@outlook.com>
2021-06-25 19:55:34 -0300

    Create PropTypes the movieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -11,5 +12,9 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;

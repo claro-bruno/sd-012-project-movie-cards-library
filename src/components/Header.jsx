38575e3a7069b1d99017c93e3fe6687b3ee59618
Augusto <augustocm2002@yahoo.com.br>
2021-06-25 15:25:41 -0300

    Fazendo requisito 1 e 2 - Criando componente Header e inserindo texto dentro de h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
 // implement Header component here
 // Iniciando projeto!
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;

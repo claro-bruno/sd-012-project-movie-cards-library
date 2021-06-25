60a4010ebb5569ef632da8665213276b9d2591bf
David Azevedo <zevid.dev@outlook.com>
2021-06-25 19:10:50 -0300

    Render MovieCard and pass data movie

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const {movies} = this.props;
     return(
       <main className="movie-list">
-        <p>movieList</p>
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </main>
     )
   }

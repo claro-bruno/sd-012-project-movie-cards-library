a55853fa6c6f70e4a0777a2de9a7228f8953c980
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-25 09:26:40 -0300

    requisito 3, 4 e 5 feitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <li> Olá</li>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
 // implement MovieList component here
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <ul>
+        {
+          movies.map((movie) => <MovieCard key={ movie.title } />)
+        }
+      </ul>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array,
+}.isRequired;
+
+export default MovieList;

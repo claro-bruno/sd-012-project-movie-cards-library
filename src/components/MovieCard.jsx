31888d46811e039937716f53a991df7d4855af16
David Azevedo <zevid.dev@outlook.com>
2021-06-25 19:25:55 -0300

    Done requirement 10 - Render the movie synopsis  inside p tag

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,10 +5,11 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     return (
       <div>
-        <section className="movie-card" key={ movie.title }>
+        <section className="movie-card movie-card-body" key={ movie.title }>
           <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
           <h4 className="movie-card-title">{ movie.title }</h4>
           <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
         </section>
       </div>
     );

538237d2ba7292824ede980f5970feaaf80be32c
David Azevedo <zevid.dev@outlook.com>
2021-06-25 19:17:23 -0300

    Done requirement 8 - Render the movie subtitle  inside h5 tag

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,8 +6,9 @@ class MovieCard extends React.Component {
     return(
       <div>
         <section className="movie-card" key={movie.title}>
-          <h4 className="movie-card-title">{movie.title}</h4>
           <img className="movie-card-image" src={movie.imagePath} alt={movie.title} />
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
         </section>
       </div>
     )

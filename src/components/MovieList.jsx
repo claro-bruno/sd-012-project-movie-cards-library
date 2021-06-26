5b2e9833de5bba2de80179235da5c517df4ddfb6
Augusto <augustocm2002@yahoo.com.br>
2021-06-26 12:14:17 -0300

    Fazendo requisito 6 - Criando componente MovieCard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,14 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return (<div> </div>);
+    return (
+      <div>
+        <MovieCard />
+      </div>
+    );
   }
 }
 export default MovieList;

79899ecc8049eb64701c290e17b2be3365481bc7
Adryan Rosa <adryanrosa374@gmail.com>
2021-06-26 00:58:38 -0300

    Cria componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,10 +4,14 @@ export class MovieCard extends Component {
   render() {
     const { movie } = this.props;
     return (
-      <>
-        <span>{movie.title}</span>
-        <br />
-      </>
+      <div className="movie-card">
+        <img className="movie-card-image" src={ movie.imagePath } alt="" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+        </div>
+      </div>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,11 @@ import MovieCard from './MovieCard';
 export class MovieList extends Component {
   render() {
     const { movies } = this.props;
-    return (movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
   }
 }
 

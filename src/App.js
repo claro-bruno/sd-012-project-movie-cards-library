ef6af3b8b7f584cfda1cc62d17302a73aedb3702
gscuri <gscuri6@hotmail.com>
2021-06-26 18:57:54 -0300

    exercicio 3 e 16

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieList />
+      </div>
+    );
+  }
 }
 
 export default App;

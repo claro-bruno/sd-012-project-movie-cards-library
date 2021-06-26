3c30912c7e15c833e88515191f83614f02fad720
gscuri <gscuri6@hotmail.com>
2021-06-26 19:13:21 -0300

    exercicio 4 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <section>
+        <MovieCard />
+      </section>
+    );
+  }
+}
+
+export default MovieCard;

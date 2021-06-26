b7700a27950be3584e52827aea7575474507c7e2
Adryan Rosa <adryanrosa374@gmail.com>
2021-06-26 00:46:58 -0300

    Cria componente MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+export class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <>
+        <span>{movie.title}</span>
+        <br />
+      </>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+export class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
+  }
+}
+
+export default MovieList;

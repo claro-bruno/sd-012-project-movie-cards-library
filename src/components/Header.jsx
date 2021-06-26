bde6103b5e4e979037ec2e823c2f0853b11e4643
Álvaro Palma <palma.alvaro@outlook.com>
2021-06-26 15:45:38 -0300

    Corrige lint requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return <header></header>
-    }
+  render() {
+    return <header />;
+  }
 }
 
 export default Header;

891214d352b0b82f36d08c3a3fe087471b06dc66
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-27 11:16:57 -0300

    Resolve o requisito 16 - MovieList App

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
 
     return (
-      movies.map((movie) => <MovieCard movie={ movies } key={ movie.title } />)
+      movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)
     );
   }
 }

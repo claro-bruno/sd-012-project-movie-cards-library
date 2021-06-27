a68cd96fd0df7f5e45285fb8e9c485d6a65446b2
Adryan Rosa <adryanrosa374@gmail.com>
2021-06-27 13:27:00 -0300

    Adiciona propTypes a MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,4 +18,14 @@ class MovieCard extends Component {
   }
 }
 
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
 export default MovieCard;

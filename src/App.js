7190d80e15d1e22b1358a8636ed1bf4961620db1
Augusto <augustocm2002@yahoo.com.br>
2021-06-27 23:18:46 -0300

    Fazendo requisito 12

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+// import Rating from './components/Rating';
 import movies from './data';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList movies={ movies } />
+      {/* <Rating /> */}
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪 */}
     </div>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,25 @@
 // implement Rating component here
 import React from 'react';
+import PropType from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return (<div> </div>);
+    const { rating } = this.props;
+    return (
+      <div className="rating">
+        <p>{rating}</p>
+      </div>);
   }
 }
+
+Rating.propTypes = {
+  rating: PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    rating: PropType.number,
+    imagePath: PropType.string,
+  }).isRequired,
+};
+
 export default Rating;

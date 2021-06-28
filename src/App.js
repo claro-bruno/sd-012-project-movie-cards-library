383b5db0470225d35ba5c26af15a4a4be5c6fd0a
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-06-28 19:24:28 -0300

    Requisitos 1 a 3 completos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Data from './data';
 
 function App() {
+  const movies = Data;
+
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header movies={movies} />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>Movie Cards Library</header>
+        )
+    }
+}
+
+export default Header;

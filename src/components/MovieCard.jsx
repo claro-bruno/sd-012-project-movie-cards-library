0e72c89c413204f8b91a40290078df05a522a222
martinsph <ph.mmartins@gmail.com>
2021-06-28 20:25:24 -0300

    add requisitos que nao envolvem ratings

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,27 @@
-// implement MovieCard component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <section>
+        <img src={ movie.imagePath } alt="movie" />
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    imagePath: PropType.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
-// implement MovieList component here
+import React from 'react';
+import PropType from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {
+          movies.map((item) => <MovieCard key={ item.title } movie={ item } />)
+        }
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropType.arrayOf(PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    imagePath: PropType.string,
+  })).isRequired,
+};
+
+export default MovieList;

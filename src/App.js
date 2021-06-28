bed92ef8f1cd81faa25a058b1e1f68219c9ec90e
PeDD1 <pedrohr0909@gmail.com>
2021-06-28 14:52:55 -0300

    Criando imports em App.js e trocando para class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
+import Rating from './components/Rating';
 
-function App() {
-  return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+        {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+        Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      </div>
+    )
+  };
 }
 
 export default App;

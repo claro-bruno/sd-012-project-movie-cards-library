ab1ee49e9d93059e457805d66b7b4ca5c5f8a279
Stefano <stefanooliveira1@gmail.com>
2021-06-27 21:54:41 -0300

    last commit fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 // implement Header component here
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 class Header extends Component {
-  render(){
+  render() {
     return (
       <header>
         <h1>Movie Cards Library</h1>
       </header>
-    )
+    );
   }
 }
 

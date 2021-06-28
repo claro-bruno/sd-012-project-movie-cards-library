fc7d21de1c162b02eb96ec8c1dde3f7a52af2e85
Álvaro Palma <palma.alvaro@outlook.com>
2021-06-28 15:50:48 -0300

    Retira estrutura <section> de MoveList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,9 +5,7 @@ import movies from '../data';
 class MovieList extends React.Component {
   render() {
     return (
-      <section className="movie-list">
-        { movies.map((movie) => <MovieCard movies={ movie } key={ movie.title } />)}
-      </section>
+      movies.map((movie) => <MovieCard movies={ movie } key={ movie.title } />)
     );
   }
 }

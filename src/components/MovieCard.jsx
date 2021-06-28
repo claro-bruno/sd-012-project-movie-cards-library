7ca1a99bd5e7820659f0ad2c52459ce197da40f0
Ed Santos <edsantos2822@gmail.com>
2021-06-28 01:34:08 -0300

    Finalizando - Requisitos 11 e 13 - Criando componente Rating e renderizando na tela

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class MovieCard extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating />
       </article>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <span>só de test</span>
+    );
+  }
+}
+
+export default Rating;

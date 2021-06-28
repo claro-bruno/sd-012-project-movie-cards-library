a80923bc2fe59eb9fdce38ee1dd9fe7022449c29
gscuri <gscuri6@hotmail.com>
2021-06-28 00:41:57 -0300

    exercicio 10 ajustado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,7 +10,6 @@ class MovieCard extends React.Component {
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
-        <p>{movie.rating}</p>
         <img src={ movie.imagePath } alt={ movie.title } />
       </section>
     );

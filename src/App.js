0f44a24d8d48e7b5a156a13fc08535a474d91e8d
PeDD1 <pedrohr0909@gmail.com>
2021-06-28 15:31:45 -0300

    Elaborado Map em MovieList com import PropTypes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import MovieData from './data'
+import catalog from './data'
 
 class App extends React.Component {
   render() {
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+    render() {
+        const { movies } = this.props;
+        return(
+         <section>
+             { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+         </section>
+        )
+    }
+}
+
+MovieList.propTypes = {
+    movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;

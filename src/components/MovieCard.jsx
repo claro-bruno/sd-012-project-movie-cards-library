859ac6f56a6fb42f7114664ec43af323b9a1ec87
Henrique Remedios <henriqueremedios@gmail.com>
2021-06-27 23:05:24 -0300

    Renderiza a imagem do filme dentro de uma tag img, o título do filme dentro de uma tag h4, o subtítulo do filme dentro de uma tag h5 e a sinopse do filme dentro de uma tag p

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,12 @@ class MovieCard extends Component {
   render() {
     const { movie } = this.props;
     return (
-      <li>{ movie.title }</li>
+      <li>
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        <img src={ movie.imagePath } alt={ movie.title } />
+      </li>
     );
   }
 }

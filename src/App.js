4beb82fa52f71a83f3772b68d56703d85a46e25d
PeDD1 <pedrohr0909@gmail.com>
2021-06-28 14:59:52 -0300

    Adicionado Header em App e renderizado

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends React.Component {
   render() {
     return(
       <div className="App">
-        {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-        Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+        <Header />
       </div>
     )
   };

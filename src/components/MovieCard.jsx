d381d9e572d52c873fde391089a3ac6843711d44
PeDD1 <pedrohr0909@gmail.com>
2021-06-28 15:41:44 -0300

    MovieCard em andamento com Props e Imports prontos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+    render() {
+        const { movie } = this.props;
+        const { title, subtitle, storyline, imagePath, rating } = movie;
+    }
+}
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        imagePath: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default MovieCard;

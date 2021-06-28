ee8a065afd9b7a56a073d9e1b64929674f0bc6a7
Wadson <wadson.dev92@gmail.com>
2021-06-28 12:32:42 -0300

    Rating criado e finalizado

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Rating() {
+export default function Rating(props) {
+  const { rating } = props;
   return (
-    <div>
-      <h1>Rating</h1>
-    </div>
+    <div className="rating">{ rating }</div>
   );
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+}.isRequired;

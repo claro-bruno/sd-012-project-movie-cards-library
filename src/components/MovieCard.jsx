69f1a5d1e3eceb643e6b4c11bb6cd433b0552570
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-06-28 17:45:19 -0300

    Requisitos 1 e 2.Criando e renderizando um componente Header

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    
+    return(
+
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import Moviecard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <Moviecard movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;

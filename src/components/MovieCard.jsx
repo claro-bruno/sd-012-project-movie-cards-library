1672915ea88e01231d6efecc06ec08bc1a498905
Ed Santos <edsantos2822@gmail.com>
2021-06-28 01:15:07 -0300

    Finalizando - Requisito 10 - Renderizando as sinopses e corrigindo propTypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,20 +4,26 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath, subtitle } = movie;
+    const { title, imagePath, subtitle, storyline } = movie;
 
     return (
       <article>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </article>
     );
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
 };
 
 export default MovieCard;

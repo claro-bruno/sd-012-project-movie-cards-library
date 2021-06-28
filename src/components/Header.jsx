2ca97b13c8c41a37591ef4cf4f48ca71bbb643b3
AlyssonSene <alysson_silva@outlook.com>
2021-06-28 15:21:32 -0300

    Iniciando projeto + requisitos 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+          <header>
+            <h1>
+              Movie Cards Library
+            </h1>
+          </header>
+      </div>
+    );
+  }
+}

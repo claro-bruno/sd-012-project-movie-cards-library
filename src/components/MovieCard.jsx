85dc6c2e83814d87dfe6cfa2df69f2256dbb50ff
PeDD1 <pedrohr0909@gmail.com>
2021-06-28 16:01:45 -0300

    Criado html do MovieCard s/ o componente Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,7 +10,12 @@ class MovieCard extends React.Component {
 
         return (
             <div className='movie-card'>
-                <div></div>
+                <div className='movie-card-body'>
+                    <img className='movie-card-image' src={ imagePath } alt={ title } />
+                    <h4 className='movie-card-title'>{ title }</h4>
+                    <h5 className='movie-card-subtitle'>{ subtitle }</h5>
+                    <p className='movie-card-storylin'>{ storyline }</p>
+                </div>
             </div>
         );
     }

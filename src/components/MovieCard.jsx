a282f4298d52e17a2451b32a6c3c56233faac84c
Henrique Remedios <henriqueremedios@gmail.com>
2021-06-27 23:53:56 -0300

    Cria um componente <Rating />, Renderize a nota de um filme dentro de Rating, Renderiza o componente <Rating /> dentro de <MovieCard /> e Passa como prop para o componente <Rating /> o atributo rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
     return (
       <li>
+        <img src={ movie.imagePath } alt={ movie.title } />
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
         <p>{ movie.storyline }</p>
-        <img src={ movie.imagePath } alt={ movie.title } />
+        <Rating rating={ movie.rating } />
       </li>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="rating">{ rating }</div>
+    );
+  }
+}
+Rating.propTypes = PropTypes.number;
+export default Rating;

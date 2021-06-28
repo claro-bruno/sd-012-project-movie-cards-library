6c773c8dde354b06fcaf0d43ca5011bf7e61fbef
Ed Santos <edsantos2822@gmail.com>
2021-06-28 00:50:47 -0300

    Finalizando - Requisito 9 - Renderizando subtitulos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath } = movie;
+    const { title, imagePath, subtitle } = movie;
 
     return (
       <article>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
       </article>
     );
   }

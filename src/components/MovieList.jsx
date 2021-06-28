56a646356dbac73ef261aa69b0c9d06982130c9d
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-27 21:00:27 -0300

    ajuste dos erros dos testes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,14 +7,14 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <section>
-        {movies.map((film) => <MovieCard key={ film.title } movies={ film } />)}
+        {movies.map((film) => <MovieCard key={ film.title } movie={ film } />)}
       </section>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.objectOf(PropTypes.string).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,12 +4,18 @@ import PropTypes from 'prop-types';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    return <span className="rating">{rating}</span>;
+    return <span className="rating">{ rating }</span>;
   }
 }
 
+// Para resolver erro no teste: 'rating undefined' consultei sobre Valores Padrão de Props em:
+// https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html#gatsby-focus-wrapper
+Rating.defaultProps = {
+  rating: 0,
+};
+
 Rating.propTypes = {
-  rating: PropTypes.number.isRequired,
+  rating: PropTypes.number,
 };
 
 export default Rating;

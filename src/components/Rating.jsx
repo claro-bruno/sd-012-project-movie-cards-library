93190798ce65d297d09cb0215d610e5b230f2aa4
martinsph <ph.mmartins@gmail.com>
2021-06-28 23:31:25 -0300

    add requisitos de rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
-// implement Rating component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <section>
+        <p className="rating">{rating}</p>
+      </section>
+    );
+  }
+}
+
+Rating.propTypes = { rating: PropType.number }.isRequired;
+
+export default Rating;

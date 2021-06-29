13de8621f0fa7b55af2dfe7db1c5718982154d79
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-06-28 21:58:48 -0300

    implementa componente Rating e adiciona prop-types

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,22 @@
-// implement Rating component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <span className="rating">
+          {rating}
+        </span>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropType.shape({
+    rating: PropType.number }).isRequired,
+};
+
+export default Rating;

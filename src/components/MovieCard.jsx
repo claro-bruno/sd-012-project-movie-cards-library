0b0c22be3637c3181f0e919471039553eb6c5115
Josimar Schneider <josimarschneider@gmail.com>
2021-06-28 23:12:29 -0400

    14 - Passe como prop para o componente <Rating/> o atributo rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Rating from './Rating';
-class MovieCard extends React.Component {
-    render() {
-        const { movie } = this.props;
-        return (
-            <section>
-                <h4>{ movie.title }</h4>
-                <h5>{ movie.subtitle }</h5>
-                <p>{ movie.storyline }</p>
-                <Rating />
-                <img src={ movie.imagePath } alt="olá" rating = {movie.rating}/>
-            </section>
-        )
 
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <section>
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        < Rating rating = {movie.rating}/>
+        <img src={ movie.imagePath } alt=""/>
+      </section>
+    )
+      
     };
 
 }

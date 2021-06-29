1482532b3e2b66bbbc29b45925912f2536b5fd7e
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-06-29 12:44:03 -0300

    requisito 4 - implementa componente movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+import MoviCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      movies.map((movie) => <MoviCard /> )
+    )
+  }
+}

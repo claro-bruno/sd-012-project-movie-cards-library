d4cf9fabad94e54f53fb5bb5b465a49f04e23574
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-28 21:06:56 -0300

    Cria componente MovieCard e Renderiza MovieList dentro dele

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component{
     render(){
-        
+        const { Movies } = this.props;
+        return ( 
+          Movies.map((item) =>  <MovieCard AllMovies={item} />)
+        )
     }
 }
 

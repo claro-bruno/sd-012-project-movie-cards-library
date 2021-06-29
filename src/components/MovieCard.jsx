b1bb6edc868255a3d0af3e7c1c607bd18b52f511
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-06-29 19:41:38 -0300

    Fin Req 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,9 @@
 // implement MovieCard component here
 import React from 'react';
-
+import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating} = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
         <img src={ imagePath } alt="Imagem do filme." />

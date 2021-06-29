0b18c23cfdffe4c8559c3a727f04d8af12a6173a
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-06-29 13:28:05 -0300

    Test de renderizaçao]

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props;
+    const { movies } = this.props;
 
     return (
       <main>

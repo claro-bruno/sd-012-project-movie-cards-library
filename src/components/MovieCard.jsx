2d4339f0c69e688e4818f5442f6046f9eb0a01a9
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-28 21:31:15 -0300

    Renderiza o Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,6 +10,7 @@ class MovieCard extends React.Component{
                 <h4>{movie.title}</h4>
                 <h5>{movie.subtitle}</h5>
                 <p>{movie.storyline}</p>
+                <Rating rating={movie.rating}/>
             </div>
         );
     }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Rating extends React.Component{
     render(){
-        
+        const { rating } = this.props;
+        return (
+        <div>
+            <p className='rating'>
+                Rating {rating}
+            </p>
+        </div>
+        );
     }
 }
 

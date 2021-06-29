19aae3a83e424731e22fd4b6929af1e10ad15d70
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-06-28 23:20:32 -0300

    Fin req1

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+import { render } from 'enzyme';
+import movies from './data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <MovieList {this.props.movies} />
+    );
+  }
+}
+
+export default MovieList;

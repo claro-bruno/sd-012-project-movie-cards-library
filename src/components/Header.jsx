64022ed40d5df6cee1d15644697cbc6ba975f00a
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-06-29 12:36:12 -0300

    requisito 2 - implementa componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
 // implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>Movie Cards Library</h1>
+    )
+  }
+}

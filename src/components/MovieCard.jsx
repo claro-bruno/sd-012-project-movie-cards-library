ac548f8b0579ed073cf2e2e5860ee271d8be8506
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-06-29 16:54:19 -0300

    Terminando requesitos 3 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <section className="cards">
         <img src={ imagePath } alt={ title } />
         <div>
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
         </div>
       </section>
     );

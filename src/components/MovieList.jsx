64b80c5b00c09b23faf8940321de920be1d6705d
Pedro Resck <pedroresck1@gmail.com>
2021-06-30 12:23:31 -0300

    Requisitos 4 e 5 - feitos: renderizar MovieCard dentro de MovieList e passar uma key para cada MovieCard renderizado, utilizando HOF Map.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <div> </div>;
+    const { movies } = this.props;
+
+    return (movies.map((movie) => <MovieCard key={ movie.title } />));
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default MovieList;

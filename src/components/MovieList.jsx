1d688d3a7fbf9df2a053efa4c96c69f8b32e4542
Gutemberg <gttj2010@hotmail.com>
2021-06-30 15:42:17 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        {/* <ul>movies</ul> */}
+      </section>
+    );
+  }
+}
+
+export default MovieList;

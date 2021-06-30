2c8024dd1effb98871dbfb83ef87d1bcb5955110
Gutemberg <gttj2010@hotmail.com>
2021-06-30 15:16:29 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <header h1="Movie Cards Library" />
+  }
+}
+
+export default Header;

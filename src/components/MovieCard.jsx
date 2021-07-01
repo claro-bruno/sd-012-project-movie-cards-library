8a658fba43bdfc133303cc31c2938f487cba713f
Pedro Resck <pedroresck1@gmail.com>
2021-06-30 23:01:59 -0300

    Requisitos 7 - 8 - 9 - 10 : feitos - renderizar img / titulo do filme em um h4 / subtitulo em um h5 / descricao do filme em um p

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return <section> </section>;
+    const { movie } = this.props;
+    console.log(this.props);
+    return (
+      <section>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>
+          { movie.title }
+        </h4>
+        <h5>
+          {movie.subtitle}
+        </h5>
+        <p>
+          {movie.storyline}
+        </p>
+      </section>);
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,8 +5,12 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-
-    return (movies.map((movie) => <MovieCard key={ movie.title } />));
+    console.log(movies);
+    return (
+      <section>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </section>
+    );
   }
 }
 

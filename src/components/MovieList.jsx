6c932c9441efe4bb134b6b269d5a1fd3aa3fddc8
EmmanuelBMS <acquabriall2020@gmail.com>
2021-07-03 17:51:59 -0300

    resove req 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <p>{ movies }</p>
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
+
+export default MovieList;

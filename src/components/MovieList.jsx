01e8ba4b369dd5cfaaf40527a649277011baef0e
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-07-03 20:37:21 -0300

    Projeto concluido 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,7 @@ class MovieList extends React.Component {
   render() {
     // está acessando o valor para cada filme.
     const { movies } = this.props;
-    // pegou os dados que estava dentro de um array de objeto a gente interou nesse array um map e para cada elemento desse array a gente retornou o component MovieCard recebendo o titulo e as informações necessarias de cada filme
+    // pegou os dados que estava dentro de um array de objeto a gente interou nesse array um map e para cada elemento desse array a gente retornou o component MovieCard recebendo o titulo e as informações necessarias de cada filme.
     return (
       <div>
         {

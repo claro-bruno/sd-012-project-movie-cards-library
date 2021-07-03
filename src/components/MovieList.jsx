0fc3a0d447d59e37c39ac2d51633321ca346d6b4
EmmanuelBMS <acquabriall2020@gmail.com>
2021-07-03 19:10:24 -0300

    resolve o req 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
-        <p>{ movies }</p>
-      </div>
+      <section>
+        {movies.map(movie => <MovieCard movie={movie} key={movie.title}/>)}
+      </section>
     );
   }
 }

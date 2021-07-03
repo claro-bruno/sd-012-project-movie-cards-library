0a9dcfb85affdf0fe2f73430a6bf40a59a0db259
EmmanuelBMS <acquabriall2020@gmail.com>
2021-07-03 19:34:30 -0300

    resolve 8 9 e estiliza

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,8 @@ class MovieCard extends React.Component {
         <div className="movie-card">
           <img className="movie-card-image" src={ imagePath } alt={ title }/>
           <h4 className="movie-card-title">{ title }</h4>
-
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
         </div>
     )
   }

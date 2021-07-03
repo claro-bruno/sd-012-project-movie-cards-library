d4655d833d896bdac43352f38eedcdcaf94edd9c
EmmanuelBMS <acquabriall2020@gmail.com>
2021-07-03 19:28:19 -0300

    adiciona imagens pro req 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+      const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-    
-    );
+        <div>
+          <img src={ imagePath } alt={ title }/>
+          <h4>{ title }</h4>
+          
+        </div>
+    )
   }
 }
 

099c495f2f71501feae8160aa543ac55e52728f9
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-05 11:52:58 -0300

    Adiciona requisito 3.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        movies
+      </div>
+    );
+  }
+}
+
+export default MovieList;

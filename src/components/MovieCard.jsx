54478d00f6e6db7c3afea68883bd2b1c1acbb578
Gutemberg <gttj2010@hotmail.com>
2021-07-05 10:48:39 -0300

    Requisitos 9, 10 e 12: Refatoraçao 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,7 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
+    console.log(movie);
     return (
       <section>
         <img className="movie-card-image" src={ imagePath } alt={ title } />

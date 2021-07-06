72ca9c4f353d092c177b0597ed186250cb169631
victordg00 <victord.goncalves@outlook.com>
2021-07-06 12:31:58 -0300

    ate o ex 14 feito

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,22 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      'Batata'
+      <p className="rating">{ rating }</p>
     );
   }
 }
+
+Rating.defaultProps = {
+  rating: 8,
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
 
 export default Rating;

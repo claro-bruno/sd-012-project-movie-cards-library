38b0b3bac0345c4b0e165d19a4474f2e2a60d29e
Ana Laura <analauraalves25@gmail.com>
2021-07-05 22:55:23 -0300

    Resolve problema de lint

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import movies from './data';
+import movie from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movie={ movies } />
+      <MovieList movie={ movie } />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { title, imagePath } = movie;
     return (
       <section>
         <img src={ imagePath } alt={ title } />

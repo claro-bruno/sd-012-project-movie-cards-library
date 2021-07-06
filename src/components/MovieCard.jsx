a04caca7f8b339dbb6c2f9d652e1c81cf99e65ef
victordg00 <victord.goncalves@outlook.com>
2021-07-06 12:14:19 -0300

    correçao 5, 11 12 13 feitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,14 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { Movie: { title, subTitle, storyline, rating, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <li>
-        <img src={ imagePath } alt="Imagem do Filme" className="movie-card-image" />
-        <div>
+      <li className="movie-card">
+        <img src={ imagePath } alt="Imagem do filme" className="movie-card-image" />
+        <div className="movie-card-body">
           <h4 className="movie-card-title">{ title }</h4>
-          <h5 className="movie-card-subtitle">{ subTitle }</h5>
-          <p className="movie-card-subtitle">{ storyline }</p>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
         </div>
         <Rating rating={ rating } />
       </li>
@@ -21,9 +21,9 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  Movie: propTypes.shape({
+  movie: propTypes.shape({
     title: propTypes.string,
-    subTitle: propTypes.string,
+    subtitle: propTypes.string,
     storyline: propTypes.string,
     rating: propTypes.number,
     imagePath: propTypes.string,

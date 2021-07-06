c124d4f9786ad923f24b724769f4919ca5ebb93c
Ana Laura <analauraalves25@gmail.com>
2021-07-05 23:08:26 -0300

    Renderiza componente Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    return <section />;
+    const { rating } = this.props; 
+    return (
+      <section className="rating">
+        <p>{ rating }</p>
+      </section>
+    );
   }
 }
 

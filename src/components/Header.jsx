a1ff5ce8925ca593695f6c45c6c2384dcccb010b
Ana Laura <analauraalves25@gmail.com>
2021-07-05 21:26:57 -0300

    Renderizando header com texto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <section></section>
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     )
   }
 }

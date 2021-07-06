a5577b6a37ca54162bb5887535c445a7863fea78
Eicke Yumoto <eickeyumoto@gmail.com>
2021-07-06 16:09:07 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import Movies from './data';
+import Rating from './components/Rating';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ Movies } />
+      <Rating movies={ movies } />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MoviesList extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { rating } = this.props;
+    <section>
+      <h4>{ movie.title }</h4>
+      <h5>{ movie.subtitle }</h5>
+      <p>{ movie.storyline }</p>
+      <img src={ movie.imagePath } alt={ movie.title } />
+      <Rating rating={ movie.rating } />
+    </section>;
+  }
+}

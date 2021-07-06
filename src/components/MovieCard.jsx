96d81a3b6d853b791e2ba4f61c9716a00ac6b43c
Ana Laura <analauraalves25@gmail.com>
2021-07-05 22:59:28 -0300

    Renderiza titulo do filme num h4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,6 +6,7 @@ class MovieCard extends React.Component {
     const { title, imagePath } = movie;
     return (
       <section>
+        <h4>{ title }</h4>
         <img src={ imagePath } alt={ title } />
       </section>
     );

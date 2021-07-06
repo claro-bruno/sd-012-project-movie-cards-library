1e74b537cbcad6989cd5dbcd8ce07d616ca3354a
Ana Laura <analauraalves25@gmail.com>
2021-07-05 22:40:16 -0300

    Renderiza imagem dentro da MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <section />
+      <section>
+        <img src={ imagePath } alt={ title } />
+      </section>
     );
   }
 }

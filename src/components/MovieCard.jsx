7c71e84ce936e4c00d889d45713dc381e2677f35
Ana Laura <analauraalves25@gmail.com>
2021-07-05 23:01:20 -0300

    Renderiza sinopse dentro de uma tag p

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <section>
         <h4>{ title }</h4>
         <img src={ imagePath } alt={ title } />
         <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </section>
     );
   }

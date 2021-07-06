4bc6bbd8075d3bda1ae505c243a3e5f2083926b4
victordg00 <victord.goncalves@outlook.com>
2021-07-06 12:38:07 -0300

    req 15 e 16 feitos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
-function App() {
-  return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <App>
+        <Header />
+        <MovieList movies={ movies } />
+      </App>
+    );
+  }
 }
 
 export default App;

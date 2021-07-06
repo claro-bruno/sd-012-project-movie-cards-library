9eba8eb91e70231a29cbf69a6d4f89c04171e56e
Ana Laura <analauraalves25@gmail.com>
2021-07-05 23:00:29 -0300

    Renderiza subtitulo numa tag h5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath } = movie;
+    const { title, subtitle, imagePath } = movie;
     return (
       <section>
         <h4>{ title }</h4>
         <img src={ imagePath } alt={ title } />
+        <h5>{ subtitle }</h5>
       </section>
     );
   }

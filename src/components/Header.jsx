3e6ae16c91fbf4cdab831a6ab800a9e770a5e09b
Alice Gonçalves <galice9992@gmail.com>
2021-07-06 21:30:16 -0300

    resolvendo requisitos 1 e 2, adicionar componente e Header e renderizar no componento o texto Movie Cards Library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
-// implement Header component here
+import react from 'react';
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+       return <header>
+          <h1>Movie Cards Library</h1>
+        </header>
+    }
+}
+
+export default Header

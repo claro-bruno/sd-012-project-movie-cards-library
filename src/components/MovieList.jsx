918ae2100c2a28b137afe0901a310d68281580f5
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-07-10 15:49:54 -0300

    Modificaçoes Lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const [movies] = this.props;
     return (
       <main>
         {

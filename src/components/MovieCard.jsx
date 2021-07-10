e4c92953a5bc166e25ec9368b94c4f8e8b695d5e
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-07-10 16:36:16 -0300

    Adiciona requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <p>Lista</p>
+      <section>
+        <img src={ movie.imagePath} alt={ movie.title } />
+      </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
 export default MovieCard;

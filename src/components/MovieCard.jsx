e1f6908d38cb7c9555ffbefd0bb34aec1add0a65
Lucas Basques <lukasbasques@gmail.com>
2021-07-11 12:51:40 -0300

    Requisitos 8,9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,15 @@ import propTypes from 'prop-types';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { imagePath } = movie;
+    const { title, subtitle, storyline, imagePath } = movie;
     const text = 'movie-card-image';
+
     return (
       <div>
         <img className={ text } src={ imagePath } alt="Movie Cover" />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
   }
@@ -17,6 +21,9 @@ class MovieCard extends Component {
 MovieCard.propTypes = {
   movie: propTypes.shape({
     imagePath: propTypes.string,
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
   }).isRequired,
 };
 

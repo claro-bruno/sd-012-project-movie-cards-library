0eaa670b5917f7c012462135e32f26b52541867c
Lucas Basques <lukasbasques@gmail.com>
2021-07-11 11:11:50 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movie from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movie={ movie } />
+
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        { movies.map((movie) => (<MovieCard
+          movie={ movie }
+          key={ movie.title }
+        />))}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: propTypes.arrayOf(
+    propTypes.shape({
+      title: propTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;

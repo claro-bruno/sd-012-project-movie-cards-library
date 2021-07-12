8dbe1a80b0b47bd0135809c25052509363db8c93
Caio-Veloso <caio.veloso@gmail.com>
2021-07-12 16:47:04 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const movies = this.props;
+    return (
+      <main>
+        <section>
+          {{ movies }}
+        </section>
+      </main>
+    );
+  }
+}
+
+export default MovieList;

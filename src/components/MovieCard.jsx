8ad5dd34cb3823e44504e1ae85f21fc545f10f5a
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-18 12:22:07 -0400

    Requisitos 6 - Crie o componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types';
 
 export default class MovieCard extends Component {
   render() {
-    // const { movie } = this.props;
+    const { movie } = this.props;
 
     return (
       <div>
-        {/* <p>{ movie.title }</p>
+        <p>{ movie.title }</p>
         <p>{ movie.subtitle }</p>
-        <p>{ movie.storyline }</p> */}
+        <p>{ movie.storyline }</p>
       </div>
     );
   }
 }
 
-MovieCard.protoTypes = {
-  movie: PropTypes.object,
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
 };

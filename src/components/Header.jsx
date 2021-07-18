ee68a4b14978e67d78ab0f7f6f714468be8f6479
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-18 00:20:56 -0400

    Requisito 1.1 - Cria o componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        oi
+      </div>
+    );
+  }
+}

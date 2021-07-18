0b29545f88de0624cebc5255540f73ee1ddb746c
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-18 01:06:27 -0400

    Requisitos 4 e 5 - Renderiza o componente MovieCard e sua key dentro do MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+export default class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+        {/* <p>{ this.props.movie.title }</p>
+        <p>{ this.props.movie.subtitle }</p>
+        <p>{ this.props.movie.storyline }</p> */}
+      </div>
+    );
+  }
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   render() {
+    const rating = 4.5;
+    const ratingThree = 3;
+    const movies = [
+      {
+        title: 'Movie Title 1',
+        subtitle: 'Movie Subtitle 1',
+        storyline: 'Movie Storyline 1',
+        rating,
+        imagePath: 'images/movie_1',
+      },
+      {
+        title: 'Movie Title 2',
+        subtitle: 'Movie Subtitle 2',
+        storyline: 'Movie Storyline 2',
+        rating,
+        imagePath: 'images/movie_2',
+      },
+      {
+        title: 'Movie Title 3',
+        subtitle: 'Movie Subtitle 3',
+        storyline: 'Movie Storyline 3',
+        ratingThree,
+        imagePath: 'images/movie_3',
+      },
+    ];
+
     return (
-      <p>oi</p>
+      <div>
+        {movies.map((e) => (
+          <MovieCard
+            key={ e.title }
+            movie={ e }
+            title={ e.title }
+            subtitle={ e.subtitle }
+            storyline={ e.storyline }
+          />
+        ))}
+      </div>
     );
   }
 }

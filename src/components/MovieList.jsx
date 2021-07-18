c11d788c414ef195ad10203ea01a1f354830aff6
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-18 00:33:48 -0400

    Requisito 3 - Cria o componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+export default class MovieList extends Component {
+  render() {
+    return (
+      <p>oi</p>
+    );
+  }
+}

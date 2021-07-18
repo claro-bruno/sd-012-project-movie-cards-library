f4fca07d8a4933096fa53d18d3723e411c0a40b1
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-18 00:27:09 -0400

    Requisito 1.2 - Cria texto Movie Cards Library no header

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        oi
-      </div>
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }

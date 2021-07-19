b312d65fbfb8508bfad05fa5ae96d17d85ad1e3b
Luan Martins Lorenzato <lorenzatoluan15@outlook.com>
2021-07-19 12:34:12 -0300

    Requisitos adicionados

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,18 @@
 // implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <section className="rating">{ rating }</section>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
